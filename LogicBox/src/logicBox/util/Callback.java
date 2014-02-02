0adbd78f535fe2b93660c2a6bdc8492d3d07decf
Lee <lee.coakley@mycit.ie>
2014-02-02 14:07:46 +0000

    Added callback interfaces.

---
+++ LogicBox/src/logicBox/util/Callback.java
@@ -0,0 +1,15 @@
+
+
+
+package logicBox.util;
+
+
+
+/**
+ * Generic callback.
+ * @author Lee Coakley
+ */
+public interface Callback<T>
+{
+	public void execute();
+}
---
+++ LogicBox/src/logicBox/util/CallbackParam.java
@@ -0,0 +1,15 @@
+
+
+
+package logicBox.util;
+
+
+
+/**
+ * Generic parameterised callback.
+ * @author Lee Coakley
+ */
+public interface CallbackParam<T>
+{
+	public void execute( final T param );
+}
