8c570593c4c8a82b2df05f7b4ac4441fdc23e02c
ShaunCIT <shaun.odonovan@mycit.ie>
2014-02-05 13:20:09 +0000

    HelpMenuFrameTest: Created this class to test the F1 interception for the Help Menu.

---
+++ LogicBox/src/logicBox/gui/HelpMenuFrameTest.java
@@ -0,0 +1,12 @@
+package logicBox.gui;
+
+import javax.swing.JFrame;
+
+public class HelpMenuFrameTest extends JFrame
+{
+	public HelpMenuFrameTest() 
+	{
+		setSize(300, 300);
+		setVisible(true);
+	}
+}
