6bd6751bff913bd4d24a47e9a8654b8d1a047c98
ShaunCIT <shaun.odonovan@mycit.ie>
2014-02-05 13:19:03 +0000

    Main: Added a main class with an instance of the HelpMenuFrameTest class.

---
+++ LogicBox/src/logicBox/gui/Main.java
@@ -0,0 +1,14 @@
+package logicBox.gui;
+
+public class Main
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		HelpMenuFrameTest frame = new HelpMenuFrameTest();	
+	}
+
+}
