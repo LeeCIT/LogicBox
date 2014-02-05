d0b884a5ad4419d6789bb2499fe130e61f740334
JohnCit <john.murphy7@mycit.ie>
2014-02-05 17:57:32 +0000

    Added prototype toolbar for testing

---
+++ LogicBox/src/frameShareProto/ToolBar.java
@@ -0,0 +1,17 @@
+package frameShareProto;
+
+import javax.swing.JFrame;
+
+public class ToolBar extends JFrame{
+	
+	public ToolBar() {
+		
+		
+		
+		
+		setTitle("Demo ToolBar");
+		setSize(200, 600);
+		setVisible(true);
+		setLocationRelativeTo(null);//Loads the window in the center	
+	}
+}
