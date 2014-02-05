893e922171881da3816e0b1a1a30bedb47c3145a
ShaunCIT <shaun.odonovan@mycit.ie>
2014-02-05 13:48:09 +0000

    HelpMenu: Tested around with a KeyListener class to try to intercept the F1 key.

--- LogicBox/src/helpMenuPrototype/HelpMenu.java
+++ LogicBox/src/helpMenuPrototype/HelpMenu.java
@@ -1,11 +1,25 @@
 package helpMenuPrototype;
 
-public class HelpMenu 
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+import javax.swing.JFrame;
+
+public class HelpMenu extends JFrame
 {
 	public boolean inMenu = false;
 	
 	public HelpMenu() 
 	{
+		setSize(300, 300);
+	}
+	
+	/**
+	 * Call the HelpMenu
+	 */
+	public void callMenu()
+	{
 		
 	}
+	
 }
