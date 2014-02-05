ca9991ee7f90573b967e7bd97f85646852bf84d1
ShaunCIT <shaun.odonovan@mycit.ie>
2014-02-05 13:47:21 +0000

    HelpMenuFrameTest: Added a method to call the menu.

--- LogicBox/src/logicBox/gui/HelpMenuFrameTest.java
+++ LogicBox/src/logicBox/gui/HelpMenuFrameTest.java
@@ -1,12 +1,22 @@
 package logicBox.gui;
 
+import helpMenuPrototype.*;
 import javax.swing.JFrame;
 
 public class HelpMenuFrameTest extends JFrame
 {
+	private HelpMenu helpMenu = new HelpMenu();
+	
 	public HelpMenuFrameTest() 
 	{
 		setSize(300, 300);
 		setVisible(true);
+		
+		
+	}
+	
+	public void getHelpMenu()
+	{
+		helpMenu.callMenu();
 	}
 }
