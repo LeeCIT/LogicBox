4cf2fa870db740d55d6ad7c4b13de5baaa5d5d31
JohnCit <john.murphy7@mycit.ie>
2014-02-05 17:57:51 +0000

    Prototype start of program

---
+++ LogicBox/src/frameShareProto/Start.java
@@ -0,0 +1,18 @@
+package frameShareProto;
+
+import logicBox.gui.GuiUtil;
+
+public class Start {
+	
+	
+	public static void main(String[] args) {
+		
+		
+		GuiUtil.setNativeLookAndFeel();
+		
+		new MainFrame();
+		new ToolBar();
+		
+	}
+
+}
