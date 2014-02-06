a73ce416da8a3b7c12ca393a69925f545eadbdad
Lee <lee.coakley@mycit.ie>
2014-02-06 18:56:07 +0000

    EditorFrame: Now uses MigLayout

--- LogicBox/src/logicBox/gui/EditorFrame.java
+++ LogicBox/src/logicBox/gui/EditorFrame.java
@@ -2,10 +2,15 @@
 
 
 package logicBox.gui;
+import javax.swing.JFrame;
+import net.miginfocom.swing.MigLayout;
 
 
 
-public class EditorFrame
+public class EditorFrame extends JFrame
 {
-	
+	public EditorFrame() {
+		super();
+		setLayout( new MigLayout( "", "[grow,fill]", "[grow,fill]" ) );
+	}
 }
