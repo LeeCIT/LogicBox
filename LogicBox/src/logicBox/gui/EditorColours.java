c22f6ba347a5f3954eb5f49c80d3559cda08521b
Lee <lee.coakley@mycit.ie>
2014-02-06 18:52:34 +0000

    Added EditorColours

---
+++ LogicBox/src/logicBox/gui/EditorColours.java
@@ -0,0 +1,32 @@
+
+
+
+package logicBox.gui;
+import java.awt.Color;
+
+
+
+public class EditorColours
+{
+	public static Color background      = new Color(  55,  37,  37 );
+	public static Color grid            = new Color(  89,  51,  72 );
+	public static Color traceOff        = new Color(  58, 122,  54 );
+	public static Color traceOn         = new Color( 146, 248, 122 );
+	public static Color junctionOff     = new Color( 208, 135,   8 );
+	public static Color junctionOn      = new Color( 255, 227,  39 );
+	public static Color componentStroke = new Color( 243,  75,  99 );
+	public static Color componentFill   = new Color( 112,  18,  58 );
+	
+	
+	
+	
+	public static Color makeHighlight( Color col ) {
+		int highlight = 64;
+		
+		return new Color(
+			Math.min( col.getRed  () + highlight, 255 ),
+			Math.min( col.getGreen() + highlight, 255 ),
+			Math.min( col.getBlue () + highlight, 255 )
+		);
+	}
+}
