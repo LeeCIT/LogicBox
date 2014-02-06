482e465378439ca0a7b9aa812facf0206fac529d
ShaunCIT <shaun.odonovan@mycit.ie>
2014-02-06 08:26:16 -0800

    HelpMenu: Inner class was added to add a KeyListener to the main gui frame.

--- LogicBox/src/helpMenuPrototype/HelpMenu.java
+++ LogicBox/src/helpMenuPrototype/HelpMenu.java
@@ -1,25 +1,47 @@
 package helpMenuPrototype;
 
-import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.JFrame;
 
 public class HelpMenu extends JFrame
 {
-	public boolean inMenu = false;
-	
 	public HelpMenu() 
 	{
 		setSize(300, 300);
+		setTitle("Help Menu");
+	}
+	
+	
+	/**
+	 * Sets a KeyListener to
+	 * the passed in JFrame.
+	 * @param mainFrame
+	 */
+	public void callMenu( JFrame mainFrame )
+	{
+		mainFrame.addKeyListener( new functionListener() );
 	}
 	
 	/**
-	 * Call the HelpMenu
+	 * Inner KeyListener
+	 * class for the mainFrame
+	 * @author Shaun
 	 */
-	public void callMenu()
+	class functionListener implements KeyListener
 	{
+		@Override
+		public void keyPressed(KeyEvent e)
+		{
+			//Check if the F1 key was pressed
+			if ( e.getKeyCode() == 112 ) { setVisible(true); }
+		}
 		
+		@Override
+		public void keyReleased(KeyEvent e) {}
+		
+		@Override
+		public void keyTyped(KeyEvent e) {}
 	}
-	
 }
