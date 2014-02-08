c6c80582a1ab73d5d014d69f6b46713496eb77f0
Lee <lee.coakley@mycit.ie>
2014-02-08 20:47:15 +0000

    Component: Sim components no longer know about their graphics state.

--- LogicBox/src/logicBox/sim/Component.java
+++ LogicBox/src/logicBox/sim/Component.java
@@ -2,7 +2,6 @@
 
 
 package logicBox.sim;
-import logicBox.util.Vec2;
 
 
 
@@ -12,8 +11,5 @@ import logicBox.util.Vec2;
  */
 public abstract class Component
 {
-	public Vec2   pos;     // Position of the component for drawing.
-	public double angle;   // Angle of rotation for drawing.
-	
 	protected long simStep; // Simulation step the component was last updated on.
 }
