0fe97fc142e14bbf3570e942e451c7d51ea17edc
Lee <lee.coakley@mycit.ie>
2014-02-08 17:02:18 +0000

    Added Bbox2

---
+++ LogicBox/src/logicBox/util/Bbox2.java
@@ -0,0 +1,17 @@
+
+
+
+package logicBox.util;
+
+
+
+/**
+ * 2D Bounding box.
+ * @author Lee Coakley
+ */
+public class Bbox2 extends Region
+{
+	public Bbox2( Vec2 tl, Vec2 br ) {
+		super( tl, br );
+	}
+}
