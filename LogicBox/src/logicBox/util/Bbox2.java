50602d4c3433323ff2c71e8f90273321aef6e7d2
Lee <lee.coakley@mycit.ie>
2014-02-09 17:33:00 +0000

    Bbox2: Added contains() and overlaps()

--- LogicBox/src/logicBox/util/Bbox2.java
+++ LogicBox/src/logicBox/util/Bbox2.java
@@ -14,4 +14,22 @@ public class Bbox2 extends Region
 	public Bbox2( Vec2 tl, Vec2 br ) {
 		super( tl, br );
 	}
+	
+	
+	
+	public boolean contains( Bbox2 other ) {
+		return other.tl.x >= tl.x
+			&& other.tl.y >= tl.y
+			&& other.br.x <= br.x
+			&& other.br.y <= br.y;
+	}
+
+
+
+	public boolean overlaps( Bbox2 other ) {
+		return other.tl.x <= br.x
+			&& other.tl.y <= br.y
+		    && other.br.x >= tl.x
+		    && other.br.y >= tl.y;
+	}
 }
