c81cf0ea717b1d3eb25066fbc18242a74654934d
JohnCIT <john.murphy7@mycit.ie>
2014-02-10 21:26:59 +0000

    ToolbarProto: Fixed flaw in JToolBar

--- LogicBox/src/prototypes/ToolBarProto/ToolbarFrame.java
+++ LogicBox/src/prototypes/ToolBarProto/ToolbarFrame.java
@@ -1,43 +1,73 @@
 package prototypes.ToolBarProto;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JToolBar;
+import java.awt.Component;
+import java.awt.Window;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
+import java.awt.event.HierarchyEvent;
+import java.awt.event.HierarchyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JToolBar;
+import javax.swing.SwingUtilities;
+import net.miginfocom.swing.MigLayout;
 import logicBox.gui.GuiUtil;
 import prototypes.snappingProto.SnappingPrototype;
 
 public class ToolbarFrame extends JFrame{
 	
-	private JLabel horizontalSeperator;
-	private JToolBar toolbar;
-	
-	
-	
-	
-	
+	private final JToolBar toolbar;
+	private JFrame frame = this;
 	
 	
 	public ToolbarFrame() {
 		GuiUtil.setNativeLookAndFeel();
 		
-		new JLabel();
-		toolbar = new JToolBar("Drag");
-		toolbar.addComponentListener(new SnappingPrototype(this));
-
+		toolbar = new JToolBar("LogicBox Tool bar", JToolBar.VERTICAL);
 		
 		toolbar.add(new JButton("DemoGate"));
 		toolbar.add(new JButton("XORGate"));
 		toolbar.addSeparator();
 		toolbar.add(new JButton("Bulb"));
 		
+		//toolbar.setLayout(new MigLayout());
+		
+		
+		
+		
+		
+		
+		toolbar.addComponentListener(new SnappingPrototype());		
+		
+		// Fix to a terrible flaw in JToolbar
+		toolbar.addHierarchyListener(new HierarchyListener() {			
+			public void hierarchyChanged(HierarchyEvent e) {
+					JToolBar bar = (JToolBar) e.getComponent();
+					final Window topLevel = SwingUtilities.windowForComponent(bar);
+				
+				if (topLevel instanceof JDialog) {
+					((JDialog) topLevel).addComponentListener(new SnappingPrototype(frame));
+				}    
+			}
+		});
+
+		
+		
 		add(toolbar);
 		
-		// Demo
 		
+		// Demo
+		addComponentListener(new SnappingPrototype());
 		setSize(150, 300);
 		setVisible(true);
 		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		pack();
 	}
 }
