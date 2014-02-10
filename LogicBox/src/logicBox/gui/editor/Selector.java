f6fd20d397f7f8a93e7b7ba84d848162fd8cf3b7
Lee <lee.coakley@mycit.ie>
2014-02-10 03:24:29 +0000

    Added placeholder Selector class

---
+++ LogicBox/src/logicBox/gui/editor/Selector.java
@@ -0,0 +1,15 @@
+
+
+
+package logicBox.gui.editor;
+
+
+
+/**
+ * Generates selection events for the editor.
+ * @author Lee Coakley
+ */
+public class Selector
+{
+	boolean isEnabled; // Maybe use this kind of style for all the tools, instead of a global mode.
+}
