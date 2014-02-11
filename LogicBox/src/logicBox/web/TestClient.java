8100c89dcfda59504cb27d84f7ed3822aae1d58d
Robert O' Leary <jatochnietdans@gmail.com>
2014-02-11 18:27:20 +0000

    Added basic test of the new auth methods as an example

--- LogicBox/src/logicBox/web/TestClient.java
+++ LogicBox/src/logicBox/web/TestClient.java
@@ -1,21 +1,15 @@
 package logicBox.web;
 
-import java.util.HashMap;
-import java.util.Map;
-import com.mashape.unirest.http.JsonNode;
-
 public class TestClient 
 {
 	public static void main(String[] args) 
 	{
-		WebClient wc = new WebClient("http://cloud.jatochnietdan.com/");
+		Auth a = new Auth("http://cloud.jatochnietdan.com/");
 		
-		Map<String, Object> map = new HashMap<String, Object>(); 
-		
-		map.put("email", "wow");
-		
-		JsonNode result = wc.post("register", map);
-		
-		System.out.println(result.toString());
+		if(!a.register("robert", ""))
+		{
+			for(String error : a.getErrors())
+				System.out.println(error);
+		}
 	}
 }
