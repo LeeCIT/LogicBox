a0d9261765a7b96e093d22db9b1dddb0978c1b6e
Robert O' Leary <jatochnietdans@gmail.com>
2014-02-11 21:24:39 +0000

    Added basic user info return page

--- BoxCloud/app/routes.php
+++ BoxCloud/app/routes.php
@@ -7,3 +7,8 @@ Route::get('/', function()
 
 Route::post('/register', 'AuthController@register');
 Route::post('/login', 'AuthController@login');
+
+Route::group(array('before' => 'auth', 'prefix' => 'user'), function()
+{
+	Route::get('', 'UserController@index');
+});
