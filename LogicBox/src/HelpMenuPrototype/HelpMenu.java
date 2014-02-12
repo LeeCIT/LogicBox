f4daa446560dc57f5986a3c9b7a36c8aba5e4cbe
ShaunCIT <shaun.odonovan@mycit.ie>
2014-02-12 12:09:12 -0800

    HelpMenu: TabbedPane added to contain the information for each individual gate.

--- LogicBox/src/HelpMenuPrototype/HelpMenu.java
+++ LogicBox/src/HelpMenuPrototype/HelpMenu.java
@@ -1,16 +1,30 @@
 package HelpMenuPrototype;
 
+import java.awt.Dimension;
+
 import javax.swing.*;
 
+import net.miginfocom.swing.MigLayout;
 import logicBox.sim.Component;
 
 public class HelpMenu extends JFrame
 {
 	private static Component gate;
+	private JTabbedPane gateTabs;
+	private JPanel andGate;
+	private JPanel orGate;
 	
 	public HelpMenu() 
 	{
 		setSize(300,300);
+		
+		setLayout( new MigLayout() );
+		
+		getContentPane().add(gateTabs  = new JTabbedPane());
+		gateTabs.add("& Gate", andGate = new JPanel());
+		gateTabs.add("| Gate", orGate  = new JPanel());
+		
+		setComponentDimensions();
 	}
 	
 	/**
@@ -23,6 +37,8 @@ public class HelpMenu extends JFrame
 		this.gate = gate;
 	}
 	
+	
+	
 
 	/**
 	 * Call the help menu.
@@ -30,6 +46,13 @@ public class HelpMenu extends JFrame
 	public void callMenu()
 	{
 		setVisible(true);
+	}
+	
+	
+	private void setComponentDimensions()
+	{
+		andGate.setPreferredSize(new Dimension(getSize()));
+		orGate.setPreferredSize(new Dimension(getSize()));
 	}
 	
 	public static void main(String[] args) 
