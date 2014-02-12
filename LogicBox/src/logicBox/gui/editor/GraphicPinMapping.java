157dc6db110c0360d42c84b8b5ca27e5b03d68c1
Lee <lee.coakley@mycit.ie>
2014-02-12 19:34:22 +0000

    GraphicPinMapping: Now defines pins in terms of Line2

--- LogicBox/src/logicBox/gui/editor/GraphicPinMapping.java
+++ LogicBox/src/logicBox/gui/editor/GraphicPinMapping.java
@@ -3,6 +3,7 @@
 
 package logicBox.gui.editor;
 import logicBox.sim.PinIoMode;
+import logicBox.util.Line2;
 import logicBox.util.Vec2;
 
 
@@ -10,19 +11,32 @@ import logicBox.util.Vec2;
 /**
  * Maps a pin's graphical representation to an IoMode and pin index.
  * Allows the graphical display to be mapped onto the simulator.
+ * Positions here are in the graphics's local coordinate space, centred on [0,0].
  * @author Lee Coakley
  */
 public class GraphicPinMapping
 {
-	public Vec2      pos;
+	public Line2     line;
 	public PinIoMode mode;
 	public int       index;
 	
 	
 	
-	public GraphicPinMapping( Vec2 pos, PinIoMode isInput, int index ) {
-		this.pos   = pos;
-		this.mode  = isInput;
+	public GraphicPinMapping( Line2 line, PinIoMode mode, int index ) {
+		this.line  = line;
+		this.mode  = mode;
 		this.index = index;
+	}
+	
+	
+	
+	public Vec2 getPinPosBody() {
+		return line.a;
+	}
+	
+	
+	
+	public Vec2 getPinPosEnd() {
+		return line.b;
 	}
 }
