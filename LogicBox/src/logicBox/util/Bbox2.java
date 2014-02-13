b9f7876c562a97c7241fc37d0a23b1f201fa526c
Lee <lee.coakley@mycit.ie>
2014-02-13 01:46:42 +0000

    Bbox2: Added primitive constructor

--- LogicBox/src/logicBox/util/Bbox2.java
+++ LogicBox/src/logicBox/util/Bbox2.java
@@ -17,6 +17,13 @@ public class Bbox2 extends Region
 	
 	
 	
+	public Bbox2( double tlx, double tly, double brx, double bry ) {
+		this.tl = new Vec2( tlx, tly );
+		this.br = new Vec2( brx, bry );
+	}
+	
+	
+	
 	public boolean contains( Bbox2 other ) {
 		return other.tl.x >= tl.x
 			&& other.tl.y >= tl.y
