f93577a71fcffe9d2e41a8c3b636c7fc9d730415
JohnCIT <john.murphy7@mycit.ie>
2014-02-14 12:55:12 +0000

    FileOpenProto: Have a working example It opens the old windows xp version.

---
+++ LogicBox/src/prototypes/fileOpen/FileOpen.java
@@ -0,0 +1,72 @@
+package prototypes.fileOpen;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import logicBox.gui.GuiUtil;
+
+public class FileOpen {
+
+	public FileOpen(JFrame frame) {
+		JFileChooser chooser = new JFileChooser();
+		
+		// File filter
+		FileNameExtensionFilter filter = new FileNameExtensionFilter("Logicbox", "lbx");
+		chooser.setFileFilter(filter);
+				
+		int returnVal = chooser.showOpenDialog(frame);
+		if(returnVal == JFileChooser.APPROVE_OPTION) {
+			System.out.println("You chose to open this file: " +
+					chooser.getSelectedFile().getName());
+		}
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	public static void main(String[] args) {
+		
+		GuiUtil.setNativeLookAndFeel();
+		
+		final JFrame frame = new JFrame("Open file demo");
+
+		JButton openFile = new JButton("Open file");
+
+
+		openFile.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				new FileOpen(frame);
+			}
+		});
+
+
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add("Center", openFile);
+		frame.pack();
+		frame.setVisible(true);
+		frame.setLocationRelativeTo(null);
+	}
+}
