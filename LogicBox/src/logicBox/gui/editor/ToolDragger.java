56b6a8b3159354900c26082407bc155b64d7c873
Lee <lee.coakley@mycit.ie>
2014-02-15 02:02:48 +0000

    ToolDragger updated with new attach/detach mechanism.

--- LogicBox/src/logicBox/gui/editor/ToolDragger.java
+++ LogicBox/src/logicBox/gui/editor/ToolDragger.java
@@ -3,6 +3,7 @@
 
 package logicBox.gui.editor;
 
+import java.awt.Cursor;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.List;
@@ -16,7 +17,7 @@ import logicBox.util.Vec2;
  * Drags components around.
  * How it's intended to work:
  *		- User holds down LMB
- *		- If they move the mouse more than a certain threshold, they begin dragging any underlying component
+ *		- If they move the mouse more than a certain threshold, they begin dragging the topmost underlying component
  *      - If they now press RMB, the drag is cancelled
  *      - If they release LMD again, the drag is completed
  *      - They can pan and zoom at the same time.
@@ -24,36 +25,37 @@ import logicBox.util.Vec2;
  *      - Holding ctrl flips the component.
  * @author Lee Coakley
  */
-public class ToolDragger
+public class ToolDragger extends Tool
 {
-	private double dragThreshold;
-	
-	private EditorPanel panel;
-	private EditorWorld world;
-	private Camera      cam;
-	
+	private EditorPanel     panel;
+	private EditorWorld     world;
+	private Camera          cam;
+	private double          dragThreshold;
 	private boolean         dragInitiated;
 	private boolean         dragging;
 	private Vec2            dragInitiatedAt;
 	private Vec2            dragOffset;
 	private EditorComponent draggedComponent;
+	private double          rotateStartAngle;
+	private MouseAdapter    eventListener;
 	
 	
 	
 	public ToolDragger( EditorPanel panel, EditorWorld world, Camera cam ) {
-		this.panel = panel;
-		this.world = world;
-		this.cam   = cam;
+		this.panel         = panel;
+		this.world         = world;
+		this.cam           = cam;
+		this.eventListener = createEventListener();
+		this.dragThreshold = 4;
 		
-		dragThreshold = 4;
-		
-		setupActions();
+		createEventListener();
+		attach();
 	}
 	
 	
 	
-	private void setupActions() {
-		MouseAdapter adapter = new MouseAdapter() {
+	private MouseAdapter createEventListener() {
+		return new MouseAdapter() {
 			public void mousePressed( MouseEvent ev ) {
 				if (SwingUtilities.isLeftMouseButton( ev ))
 					dragInitiate( cam.getMousePosWorld() );
@@ -67,17 +69,36 @@ public class ToolDragger
 					dragComplete();
 			}
 			
-			public void mouseMoved( MouseEvent ev ) {
-				dragMove( cam.getMousePosWorld() );
-			}
-			
 			public void mouseDragged( MouseEvent ev ) {
-				dragMove( cam.getMousePosWorld() );
+				Vec2 pos = cam.getMousePosWorld();
+				
+				if ( ! ev.isShiftDown())
+					 dragMove  ( pos );
+				else rotateMove( pos );
 			}
 		};
+	}
+
+
+
+	public void attach() {
+		if (isAttached())
+			return;
 		
-		panel.addMouseListener      ( adapter );
-		panel.addMouseMotionListener( adapter );
+		panel.addMouseListener      ( eventListener );
+		panel.addMouseMotionListener( eventListener );
+		setAttached( true );
+	}
+
+
+
+	public void detach() {
+		if ( ! isAttached())
+			return;
+		
+		panel.removeMouseListener      ( eventListener );
+		panel.removeMouseMotionListener( eventListener );
+		setAttached( false );
 	}
 	
 	
@@ -94,6 +115,7 @@ public class ToolDragger
 		dragInitiatedAt  = pos;
 		dragOffset       = ecom.pos.subtract( pos );
 		draggedComponent = ecom;
+		rotateStartAngle = ecom.angle;
 	}
 	
 	
@@ -108,29 +130,44 @@ public class ToolDragger
 		}
 		
 		if (dragging) {
-			world.move( draggedComponent, Geo.snapNear( pos.add( dragOffset ), 64 ) );
+			panel.setCursor( new Cursor(Cursor.MOVE_CURSOR) );
+			world.move( draggedComponent, Geo.snapNear( pos.add( dragOffset ), 32 ) );
 			panel.repaint();
 		}
 	}
 	
 	
 	
-	private void dragComplete() {	
-		System.out.println( "drag complete" );
-		dragInitiated = false;
-		dragging      = false;
+	protected void rotateMove( Vec2 pos ) {
+		if ( ! (dragInitiated || dragging))
+			return;
+		
+		double angle   = Geo.angleBetween( pos, draggedComponent.pos );
+		double snapped = Geo.roundToMultiple( angle, 45 );
+		draggedComponent.angle = snapped - 180;
 		panel.repaint();
 	}
 	
 	
 	
+	private void dragComplete() {	
+		dragFinishedCommon();
+	}
+	
+	
+	
 	private void dragCancel() {
-		if ( ! dragging)
-			return;
-		
 		world.move( draggedComponent, dragInitiatedAt.add(dragOffset) );
-		
-		dragging = false;
+		draggedComponent.angle = rotateStartAngle;
+		dragFinishedCommon();
+	}
+	
+	
+	
+	private void dragFinishedCommon() {
+		dragInitiated = false;
+		dragging      = false;
+		panel.setCursor( new Cursor(Cursor.DEFAULT_CURSOR) );
 		panel.repaint();
 	}
 }
