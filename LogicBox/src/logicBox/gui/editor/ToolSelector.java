87981aeb85ecf5ab1db1f1efdfb081b0be45fbc4
Lee <lee.coakley@mycit.ie>
2014-02-15 04:11:31 +0000

    ToolSelector: Placeholder

--- LogicBox/src/logicBox/gui/editor/ToolSelector.java
+++ LogicBox/src/logicBox/gui/editor/ToolSelector.java
@@ -6,10 +6,14 @@ package logicBox.gui.editor;
 
 
 /**
- * Generates selection events for the editor.
+ * Selects editor objects and manipulates them.
  * @author Lee Coakley
  */
 public class ToolSelector extends Tool
 {
-	boolean isEnabled; // Maybe use this kind of style for all the tools, instead of a global mode.
+
+	public void attach() {}
+
+	public void detach() {}
+	
 }
