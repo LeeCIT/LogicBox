c8287fdaf9f0428406c1fa269e7fd0efa43d3d65
ShaunCIT <shaun.odonovan@mycit.ie>
2014-02-15 11:22:50 -0800

    HelpMenu: Classes updated from today's lesson.

--- LogicBox/src/HelpMenuPrototype/HelpMenu.java
+++ LogicBox/src/HelpMenuPrototype/HelpMenu.java
@@ -1,153 +1,61 @@
 package HelpMenuPrototype;
 
-import java.util.HashMap;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.util.Map;
 
 import javax.swing.*;
 
-import net.miginfocom.swing.MigLayout;
 import logicBox.sim.ComponentType;
 
 public class HelpMenu extends JPanel
 {
 	private ComponentType componentType;
-	private HashMap <ComponentType, String> componentMap = new HashMap <ComponentType, String> ();
-	
-	public HelpMenu( ComponentType componentType ) 
-	{
-		this.componentType = componentType;
-		setSize(300,300);
-		setLayout( new MigLayout() );
-		callMenu();
-		displayGateInfo();
-	}
+	private Map <ComponentType, String> componentMap;
 	
 	
-	public void displayComponentInfo()
-	{
+	
+	public HelpMenu() {
 		
 	}
 	
-	/**
-	 * Display help information
-	 * based on the specified gate.
-	 */
-	private void displayGateInfo()
+	
+	public HelpMenu( Map<ComponentType, String> compMap ) 
 	{
-		switch (componentType)
-		{
-			case gateRelay:
-			{
-				System.out.println("Relay info");
-				break;
-			}
-			case gateNot:
-			{
-				System.out.println("Not gate info");
-				break;
-			}
-			case gateAnd:
-			{
-				System.out.println("And gate info");
-				break;
-			}
-			case gateNand:
-			{
-				System.out.println("Nand gate info");
-				break;
-			}
-			case gateOr:
-			{
-				System.out.println("Or gate info");
-				break;
-			}
-			case gateNor:
-			{
-				System.out.println("Nor gate info");
-				break;
-			}
-			case gateXnor:
-			{
-				System.out.println("Xnor gate info");
-				break;
-			}
-			case sourceFixed:
-			{
-				System.out.println("Source fixed info");
-				break;
-			}
-			case sourceToggle:
-			{
-				System.out.println("Source toggle info");
-				break;
-			}
-			case sourceOscillator:
-			{
-				System.out.println("Source Oscillator info");
-				break;
-			}
-			case junction:
-			{
-				System.out.println("Junction info");
-				break;
-			}
-			case trace:
-			{
-				System.out.println("Trace info");
-				break;
-			}
-			case displayLed:
-			{
-				System.out.println("LED info");
-				break;
-			}
-			case displaySevenSeg:
-			{
-				System.out.println("Seven Seg info");
-				break;
-			}
-			case mux:
-			{
-				System.out.println("Mux info");
-				break;
-			}
-			case demux:
-			{
-				System.out.println("Demux info");
-				break;
-			}
-			case flipFlopD:
-			{
-				System.out.println("D Flip Flop info");
-				break;
-			}
-			case blackBox:
-			{
-				System.out.println("Black Box info");
-				break;
-			}
-			case blackBoxPin:
-			{
-				System.out.println("Black Box Pin info");
-				break;
-			}
-			default:
-			{
-				
-				break;
-			}
+		super();
+		this.componentMap = compMap;
+		setSize(300,300);
+	}
+	
+	
+	
+	public void paintComponent( Graphics g )
+	{	
+		super.paintComponent( g );
+		g.setColor(Color.black);
+		g.drawString(getDescription(componentType), 64, 64);	
+	}
+	
+	
+	
+	
+	private String getDescription( ComponentType componentType )
+	{
+		if ( ! componentMap.containsKey(componentType) ){
+			return "Missing info";
 		}
+		
+		return componentMap.get(componentType);
 	}
 	
 	
 	
-
-	/**
-	 * Call the help menu.
-	 */
-	public void callMenu()
-	{
-		setVisible(true);
+	
+	public void setDisplayedInfo(ComponentType compType) {
+		this.componentType = compType;
+		repaint();
 	}
+	
 	
 	
 }
--- LogicBox/src/HelpMenuPrototype/MenuFrameTest.java
+++ LogicBox/src/HelpMenuPrototype/MenuFrameTest.java
@@ -1,26 +1,85 @@
+
+
+
 package HelpMenuPrototype;
 
-import javax.swing.JFrame;
 
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.HashMap;
+import java.util.Map;
+import javax.swing.JFrame;
 import net.miginfocom.swing.MigLayout;
 import logicBox.sim.ComponentType;
+import logicBox.util.Util;
+
+
 
 public class MenuFrameTest extends JFrame 
-{
-	private HelpMenu menu;
-	private ComponentType gateNot;
-	
+{	
 	public MenuFrameTest() 
-	{
-		setSize(300,300);
-		setVisible(true);
-		setLayout( new MigLayout() );
+	{	
+		HelpMenu menu = new HelpMenu( getComponentMap() );
 		
-		getContentPane().add(menu = new HelpMenu(gateNot));
+		
+		setLayout( new MigLayout("debug", "[fill,grow]", "[fill,grow]") );
+		setSize(300,300);
+		add( menu, "wrap" );
+		setVisible(true);
+		
+		
+		menu.setDisplayedInfo( ComponentType.gateAnd );
+		
+		menu.addMouseListener( new ClickTest(menu) );
 	}
 	
+	
+	
+	private static ComponentType getRandomComp() {
+		ComponentType[] components = ComponentType.values();
+		int random = Util.randomIntRange(0, components.length);
+		return components[random];
+	}
+
+	
+	
+	private Map<ComponentType, String> getComponentMap() {
+		Map<ComponentType, String> compMap = new HashMap<ComponentType, String>();
+		
+		compMap.put(ComponentType.gateAnd, "And gate info");
+		compMap.put(ComponentType.gateNot, "Not gate info");
+		
+		return compMap;
+	}
+	
+	
+
 	public static void main(String[] args) 
 	{
 		MenuFrameTest frame = new MenuFrameTest();
+		
+		
+		System.out.println( MenuFrameTest.getRandomComp() );
+	}
+	
+	
+	
+	private class ClickTest extends MouseAdapter
+	{
+		private HelpMenu menu;
+		
+		
+		public ClickTest( HelpMenu menu ) {
+			this.menu = menu;
+		}
+		
+		
+		
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			menu.setDisplayedInfo(getRandomComp());
+		}
+		
+		
 	}
 }
