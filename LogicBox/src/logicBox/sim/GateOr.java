8427126d74ffcf2274dfe079c4ba10bbc8055350
Lee <lee.coakley@mycit.ie>
2014-02-16 15:05:34 +0000

    Added GateOr class

---
+++ LogicBox/src/logicBox/sim/GateOr.java
@@ -0,0 +1,44 @@
+
+
+
+package logicBox.sim;
+
+
+
+/**
+ * Gate which outputs true if any of its inputs are true.
+ * 0,0 -> 0
+ * 0,1 -> 1
+ * 1,0 -> 1
+ * 1,1 -> 1
+ * @author Lee Coakley
+ */
+public class GateOr extends GateConfigurableInputs
+{
+	public GateOr() {
+		super( 2 );
+	}
+	
+	
+	
+	public GateOr( int inputPinCount ) {
+		super( inputPinCount );
+	}
+	
+	
+	
+	public boolean evaluate() {
+		boolean state = false;
+		
+		for (Pin pin: getPinInputs())
+			state |= pin.getState();
+		
+		return state;
+	}
+	
+	
+	
+	public String getName() {
+		return "Or gate";
+	}
+}
