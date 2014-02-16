2821ab61f759c35e3abdfd62224d7366c5a9219d
JohnCIT <john.murphy7@mycit.ie>
2014-02-16 00:38:29 +0000

    CoolMenuPrototype: Added easy way to add to the menu bar

--- LogicBox/src/prototypes/menuBar/CoolMenuBar.java
+++ LogicBox/src/prototypes/menuBar/CoolMenuBar.java
@@ -2,61 +2,113 @@
 package prototypes.menuBar;
 
 import java.awt.BorderLayout;
+import java.awt.Menu;
+import java.awt.MenuItem;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.*;
 
 import logicBox.gui.GuiUtil;
 import prototypes.snappingProto.SnappingPrototype;
 
+/**
+ * 
+ * @author John
+ *
+ */
 public class CoolMenuBar extends JMenuBar
 {
-	
-	public CoolMenuBar() {
+	public CoolMenuBar(){
 		super();
 	}
+
 	
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	/**
+	 * Pass in a list of CoolMenuItems with the first one being the heading you want at the top
+	 * e.g "File"
+	 * Then the subheadings e.g "Save"
+	 * @param items
+	 */
+	public void addHeadingAndSubHeadings(List<CoolMenuItem> items) {
+		JMenu menu = new JMenu();
+		for (CoolMenuItem item : items) {
+			if ( item.getHeading() != null ) {
+				menu.setText(item.getHeading());
+			}
+
+			if ( item.isSeparator() ) {
+				menu.addSeparator();
+			}
+
+			if ( !(item.getHeading() != null) && !(item.isSeparator()) ) {
+				menu.add(item);
+			}			
+		}
+		add(menu);
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 	public static void main(String[] args) {
 		GuiUtil.setNativeLookAndFeel();
-		JFrame frame  = new JFrame("Demo");		
-		JMenuBar menu = new JMenuBar();		
+		JFrame   frame = new JFrame("Demo");					
 		
-		JMenu file = new JMenu("File");
-		file.add(new JMenuItem("Save"));
-		file.add(new JMenuItem("Save As..."));
-		file.add(new JSeparator());
-		file.add(new JMenuItem("Load..."));
+		// Make first then add list with the headings and sub headings
+		CoolMenuBar menuBar = new CoolMenuBar();
 		
-		JMenu view = new JMenu("View");
-		view.add(new JMenuItem("Dock toolbox"));
+		// New way of adding
+		CoolMenuItem fileH	= new CoolMenuItem(null,        "File",null, '0', null, false);
+		CoolMenuItem save   = new CoolMenuItem("Save", 		 null, null, 's', null, false);
+		CoolMenuItem saveAs = new CoolMenuItem("Save As...", null, null, '0', null, false);
+		CoolMenuItem sep    = new CoolMenuItem(null, 		 null, null, '0', null, true);
+		CoolMenuItem open   = new CoolMenuItem("Open", 		 null, null, 'o', null, false);
+
+		List<CoolMenuItem> list = new ArrayList<>();
+		list.add(fileH);
+		list.add(save);
+		list.add(saveAs);
+		list.add(sep);
+		list.add(open);
+
+
+		// New way of adding
+		CoolMenuItem viewH	= new CoolMenuItem(null,          "View",null, '0', null, false);
+		CoolMenuItem dock   = new CoolMenuItem("DockToolbar", null, null, 's', null, false);
+		CoolMenuItem demo   = new CoolMenuItem("demo",        null, null, '0', null, false);
+
+		List<CoolMenuItem> viewList = new ArrayList<>();
+		viewList.add(viewH);
+		viewList.add(dock);
+		viewList.add(demo);
+
 		
-		JMenu edit = new JMenu("Edit");
-		view.add(new JMenuItem("Colour scheme"));
-		
-		menu.add(file);
-		menu.add(view);
-		menu.add(edit);
-		
-		frame.add(menu, BorderLayout.NORTH);
-		
+		menuBar.addHeadingAndSubHeadings(list);
+		menuBar.addHeadingAndSubHeadings(viewList);
+
+		frame.add(menuBar, BorderLayout.NORTH);
+
+
 		// Demo
 		frame.addComponentListener(new SnappingPrototype());
 		frame.setSize(600, 600);
--- LogicBox/src/prototypes/menuBar/CoolMenuItem.java
+++ LogicBox/src/prototypes/menuBar/CoolMenuItem.java
@@ -2,16 +2,25 @@ package prototypes.menuBar;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.Icon;
-import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 
 import logicBox.util.Callback;
 
+/**
+ * Make cool items to pass into the coolMenu extended methods
+ * A heading will be the top level view e.g "File" When making one leave all other fields to null and separator to false 
+ * To use a separator make isSeparator true and leave other prams to null
+ * @author John
+ *
+ */
 public class CoolMenuItem extends JMenuItem
 {
-	public CoolMenuItem (Icon icon, String title, final Callback callback, char mnemonic) {
+	
+	private String heading;
+	private boolean isSeparator;
+	
+	public CoolMenuItem (String title, String heading, final Callback callback, char mnemonic, Icon icon, boolean isSeparator) {
 		super();
 		
 		if ( title != null ){
@@ -30,5 +39,16 @@ public class CoolMenuItem extends JMenuItem
 				}
 			});
 		}
-	}	
+		
+		this.isSeparator = isSeparator;
+		this.heading 	 = heading;
+	}
+
+	public String getHeading() {
+		return heading;
+	}
+
+	public boolean isSeparator() {
+		return isSeparator;
+	}
 }
