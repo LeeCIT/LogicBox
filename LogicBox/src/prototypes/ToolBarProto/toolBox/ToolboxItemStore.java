28f63cad23b7237a7f2fa8860526777de9792297
JohnCIT <john.murphy7@mycit.ie>
2014-02-16 19:33:23 +0000

    ToolBox: Changed data input

--- LogicBox/src/prototypes/ToolBarProto/toolBox/ToolboxItemStore.java
+++ LogicBox/src/prototypes/ToolBarProto/toolBox/ToolboxItemStore.java
@@ -1,38 +1,20 @@
 package prototypes.ToolBarProto.toolBox;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.Icon;
 import javax.swing.JButton;
 import javax.swing.JToolBar;
-
-import logicBox.util.Callback;
 
 public class ToolboxItemStore extends ToolboxItem
 {
 	private JButton  button;
-	private Callback callback;
-	private Icon 	 icon;
+
 
 	
-	public ToolboxItemStore(JButton button, Icon icon, Callback callback) {
-		this.button		= button;
-		this.icon 		= icon;
-		this.callback 	= callback;
+	public ToolboxItemStore(JButton button) {
+		this.button	= button;
 	}
 
 
-	protected void addTo(final JToolBar toolbar) {		
-		button.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				callback.execute();					
-			}
-		});
-		
-		if (icon != null)
-			button.setIcon(icon);
-		
+	protected void addTo(final JToolBar toolbar) {				
 		toolbar.add(button);
 	}
 
