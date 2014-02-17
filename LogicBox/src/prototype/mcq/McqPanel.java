e7266b69c512942089646110b78a6a8a3c02c535
ShaunCIT <shaun.odonovan@mycit.ie>
2014-02-17 11:37:31 -0800

    McqPanel: Class created to test with some ideas.

---
+++ LogicBox/src/prototype/mcq/McqPanel.java
@@ -0,0 +1,30 @@
+
+
+
+package prototype.mcq;
+
+
+
+import java.util.ArrayList;
+import javax.swing.JPanel;
+
+
+
+public class McqPanel extends JPanel
+{
+	
+	
+	
+	private ArrayList<String> questions;
+	
+	
+	
+	public McqPanel( ArrayList<String> questions ) {
+		super();
+		this.questions = questions;
+		
+	}
+	
+	
+	
+}
