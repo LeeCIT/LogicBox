7e7597be6411d9e89853f175b6cc61312f723e1b
Lee <lee.coakley@mycit.ie>
2014-02-19 00:26:52 +0000

    Added Plexer class which mux and demux will now be derived from.

---
+++ LogicBox/src/logicBox/sim/component/Plexer.java
@@ -0,0 +1,99 @@
+
+
+
+package logicBox.sim.component;
+
+import java.util.ArrayList;
+import java.util.List;
+import logicBox.sim.Pin;
+import logicBox.sim.PinIoMode;
+import logicBox.sim.SimUtil;
+import logicBox.util.Geo;
+
+
+
+/**
+ * Defines the functionality common to the multiplexer and demultiplexer.
+ * @author Lee Coakley
+ */
+public abstract class Plexer extends ComponentActive
+{
+	protected List<Pin> pinInputs;
+	protected List<Pin> pinSelects;
+	protected List<Pin> pinOutputs;
+	
+	
+	
+	public Plexer(  ) {
+		super();
+		pinInputs  = new ArrayList<>();
+		pinSelects = new ArrayList<>();
+		pinOutputs = new ArrayList<>();
+	}
+	
+	
+	
+	protected void createPins( int inputs, int selects, int outputs ) {
+		SimUtil.addPins( pinInputs,  this, PinIoMode.input,  inputs  );
+		SimUtil.addPins( pinSelects, this, PinIoMode.input,  selects );
+		SimUtil.addPins( pinOutputs, this, PinIoMode.output, outputs );
+	}
+	
+	
+	
+	public List<Pin> getPinInputs() {
+		return pinInputs;
+	}
+	
+	
+	
+	public List<Pin> getPinSelects() {
+		return pinSelects;
+	}
+	
+	
+	
+	public List<Pin> getPinOutputs() {
+		return pinOutputs;
+	}
+	
+	
+	
+	public void update() {
+		for (Pin pin: pinOutputs)
+			pin.setState( false );
+		
+		Pin src  = getSourcePin();
+		Pin dest = getDestinationPin();
+		
+		if (src  != null
+		&&  dest != null)
+			dest.setState( src.getState() );
+	}
+	
+	
+	
+	protected abstract Pin getSourcePin();
+	
+	
+	
+	protected abstract Pin getDestinationPin();
+	
+	
+	
+	protected int computeSelectPinCount( int basisPinCount ) {
+		return (int) Math.ceil( Geo.log2(basisPinCount) );
+	}
+	
+	
+	
+	protected int decodeSelectPins() {
+		int select = 0;
+		
+		for (int i=0; i<pinSelects.size(); i++)
+			if (pinSelects.get(i).getState())
+				select |= (1 << i);
+		
+		return select;
+	}
+}
