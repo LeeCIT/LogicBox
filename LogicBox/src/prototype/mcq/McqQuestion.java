071ce329dd91897c855ebd49b174d89a5ce952df
ShaunCIT <shaun.odonovan@mycit.ie>
2014-02-22 06:18:10 -0800

    McqQuestion: Class name changed.

---
+++ LogicBox/src/prototype/mcq/McqQuestion.java
@@ -0,0 +1,58 @@
+
+
+
+package prototype.mcq;
+
+
+
+import java.util.ArrayList;
+
+
+
+public class McqQuestion {
+	
+	
+	
+	private ArrayList<String> answers;
+	private String question, correctAnswer;
+	
+	
+	
+	public McqQuestion( String question, ArrayList<String> answers, String correctAnswer ) {
+		this.answers = answers;
+		this.correctAnswer = correctAnswer;
+		this.question = question;
+		
+	}
+	
+	
+	
+	/**
+	 * Return the ArrayList of possible answers
+	 * to the question.
+	 * @return
+	 */
+	public ArrayList<String> getAnswers() {
+		return answers;
+	}
+	
+	
+	
+	/**
+	 * Return the question.
+	 * @return
+	 */
+	public String getQuestion() {
+		return question;
+	}
+	
+	
+	
+	/**
+	 * Return the correct answer.
+	 * @return
+	 */
+	public String getCorrectAnswer() {
+		return correctAnswer;
+	}
+}
