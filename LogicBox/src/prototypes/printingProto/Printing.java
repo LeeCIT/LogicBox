26e12eb0dc0fa3f58dc3cc5e461c296fae86ddb7
JohnCIT <john.murphy7@mycit.ie>
2014-02-23 15:17:15 +0000

    Printing: removed unused method

--- LogicBox/src/prototypes/printingProto/Printing.java
+++ LogicBox/src/prototypes/printingProto/Printing.java
@@ -1,14 +1,7 @@
 package prototypes.printingProto;
 
-import java.awt.Component;
-import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.geom.AffineTransform;
-import java.awt.image.AffineTransformOp;
-import java.awt.image.BufferedImage;
+import java.awt.*;
 import java.awt.print.*;
-
 import javax.swing.*;
 
 
@@ -22,15 +15,16 @@ public class Printing implements Printable{
 		if (pageIndex > 0) {
 			return NO_SUCH_PAGE;
 		}
-
-		createImage();
-		setDoubleBuffereing(false);
-		
-		Graphics2D g2d = (Graphics2D)graphics;
-	    g2d.translate(pageFormat.getImageableX(), pageFormat.getImageableY());
 	    
-		// Print the frame
-	    comp.printAll(g2d);
+		Graphics2D g2d = (Graphics2D) graphics;
+        g2d.translate(pageFormat.getImageableX(), pageFormat.getImageableY());
+		
+		// Turn of double buffering for better image quality
+		setDoubleBuffereing(false);
+		comp.printAll(g2d);
+	    
+	    // Turn it back on
+	    setDoubleBuffereing(true);
 		
 		return PAGE_EXISTS;
 	}
@@ -54,17 +48,6 @@ public class Printing implements Printable{
 				ex.printStackTrace();
 			}
 		}
-	}
-
-
-	private BufferedImage createImage() {
-		Dimension     size    = comp.getPreferredSize();
-		BufferedImage image   = new BufferedImage( size.width, size.height, BufferedImage.TYPE_INT_RGB );
-		Graphics2D    g2d     = image.createGraphics();
-		comp.paint(g2d);
-		g2d.dispose();
-		
-		return image;
 	}
 	
 	
