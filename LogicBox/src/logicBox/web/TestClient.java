1a559fa1840465b1b5cfa2922ced2d7ed4a9165c
Robert O' Leary <jatochnietdans@gmail.com>
2014-02-23 12:13:46 +0000

    Added test procedure

--- LogicBox/src/logicBox/web/TestClient.java
+++ LogicBox/src/logicBox/web/TestClient.java
@@ -4,6 +4,11 @@ public class TestClient
 {
 	public static void main(String[] args) 
 	{
-
+		RequestHandler rh = new RequestHandler();
+		RequestHandler.LoginHandler lh = rh.new LoginHandler();
+		
+		Request r = new Request("http://lvh.me/");
+		
+		r.login("demo@demo.com", "demo", lh);
 	}
 }
