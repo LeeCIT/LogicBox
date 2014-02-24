f164a1cf22cc1eb34adadb1d31d0d6c1a4bc8fd5
Lee <lee.coakley@mycit.ie>
2014-02-24 18:26:02 +0000

    Added Evaluator<T> interface

---
+++ LogicBox/src/logicBox/util/Evaluator.java
@@ -0,0 +1,15 @@
+
+
+
+package logicBox.util;
+
+
+
+/**
+ * Generic evaluator interface.
+ * Used as a value-returning callback.
+ * @author Lee Coakley
+ */
+public interface Evaluator<T> {
+	public T evaluate();
+}
