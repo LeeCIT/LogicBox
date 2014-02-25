9bc54b35acb8cb1ea10588e1fb5e6f8925d43f68
ShaunCIT <shaun.odonovan@mycit.ie>
2014-02-25 11:26:46 -0800

    CreateMCQPanel: Added JTextFields to add the question and answers to the quesion.

--- LogicBox/src/prototype/mcq/CreateMCQPanel.java
+++ LogicBox/src/prototype/mcq/CreateMCQPanel.java
@@ -5,7 +5,12 @@ package prototype.mcq;
 
 
 
-import javax.swing.JPanel;
+import java.awt.Dimension;
+import java.util.ArrayList;
+
+import javax.swing.*;
+
+import net.miginfocom.swing.MigLayout;
 
 
 
@@ -14,8 +19,52 @@ public class CreateMCQPanel extends JPanel
 	
 	
 	
-	public CreateMCQPanel() 
-	{
+	private ArrayList<JTextField> quesionFields = new ArrayList<>();
+	private JTextField questionField = new JTextField(); 
+	private int numOfAnswers = 4;
+	
+	
+	public CreateMCQPanel() {
+		
+		
 		super();
+		setLayout( new MigLayout() );
+		
+		
+		addToPanel();
 	}
+	
+	
+	
+	/**
+	 * Add the contents to the panel.
+	 */
+	private void addToPanel(){
+		
+		add ( new JLabel("Create MCQ Question"), "wrap" );
+		add ( new JLabel("Question: "), "split 2");
+		
+		questionField.setPreferredSize( new Dimension(400,20));
+		add ( questionField, "wrap" );
+		
+		add ( new JLabel("Set answers to the question: "), "wrap" );
+		
+		addAnswerFields();
+		
+		
+	}
+	
+	
+	
+	private void addAnswerFields() {
+		
+		for ( int i = 0; i < numOfAnswers; i++ )
+		{
+			quesionFields.add( new JTextField() );
+			add( quesionFields.get(i), "wrap" );
+			quesionFields.get(i).setPreferredSize( new Dimension(100, 10));
+		}
+		
+	}
+	
 }
