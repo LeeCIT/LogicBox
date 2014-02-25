b93e5227c0e23c1a508c80912f386117978395b5
ShaunCIT <shaun.odonovan@mycit.ie>
2014-02-25 11:28:18 -0800

    CreateMCQFrame: main method added for testing.

--- LogicBox/src/prototype/mcq/CreateMCQFrame.java
+++ LogicBox/src/prototype/mcq/CreateMCQFrame.java
@@ -6,6 +6,7 @@ package prototype.mcq;
 
 
 import javax.swing.JFrame;
+import net.miginfocom.swing.MigLayout;
 
 
 
@@ -17,8 +18,18 @@ public class CreateMCQFrame extends JFrame
 	public CreateMCQFrame() 
 	{
 		
+		
+		setTitle("Create MCQ"); //Setting the title.
+		setLayout( new MigLayout() );
+		add ( new CreateMCQPanel() ); //Add the panel to the frame.
+		
+		setVisible(true);
+		pack();
+		
 	}
 	
 	
-	
+	public static void main(String[] args) {
+		CreateMCQFrame mcq = new CreateMCQFrame();
+	}
 }
