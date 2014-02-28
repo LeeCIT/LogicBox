15c1c02c01472bb6f846d5f135fa412da022bc2a
Lee <lee.coakley@mycit.ie>
2014-02-28 15:30:14 +0000

    Added TestCounter class: simulates a 4-bit counter with JK flip-flops

---
+++ LogicBox/src/logicBox/sim/TestCounter.java
@@ -0,0 +1,52 @@
+
+
+
+package logicBox.sim;
+
+import logicBox.sim.component.*;
+
+
+
+public class TestCounter
+{
+	public static void main( String[] args ) {
+		SourceToggle osc = new SourceToggle( false );
+		
+		FlipFlopJK[] ffs = {
+			new FlipFlopJK(),
+			new FlipFlopJK(),
+			new FlipFlopJK(),
+			new FlipFlopJK()
+		};
+		
+		for (FlipFlopJK f: ffs) {
+			f.getPinJ().setState( true );
+			f.getPinK().setState( true );
+		}
+		
+		for (int i=0; i<64; i++) {
+			for (int r=0; r<2; r++) {
+				ffs[0].getPinC().setState( osc.getState() );
+				ffs[0].update();
+				
+				for (int z=0; z<ffs.length-1; z++) {
+					FlipFlopJK cur  = ffs[ z     ];
+					FlipFlopJK next = ffs[ z + 1 ];
+					
+					next.getPinC().setState( cur.getPinQ().getState() );
+					next.update();
+				}
+				
+				osc.toggleState();
+			}
+			
+			int value = 0;
+			for (int z=ffs.length-1; z>=0; z--) {
+				int level = ffs[z].getPinQ().getState() ? 1 : 0;
+				value |= level << z;
+			}
+			
+			System.out.println( ~value & 0xF ); // Count up instead of down, by complement, then chop irrelevant bits
+		}
+	}
+}
