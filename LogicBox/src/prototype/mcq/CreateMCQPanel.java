c38b529be4b0a224a1d7de83f7fdba5dc53db2cd
ShaunCIT <shaun.odonovan@mycit.ie>
2014-02-28 17:19:54 -0800

    CreateMCQPanel: User can no select which answer is the correct one.

--- LogicBox/src/prototype/mcq/CreateMCQPanel.java
+++ LogicBox/src/prototype/mcq/CreateMCQPanel.java
@@ -6,6 +6,8 @@ package prototype.mcq;
 
 
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 import javax.swing.*;
@@ -23,6 +25,9 @@ public class CreateMCQPanel extends JPanel
 	private JTextField questionField = new JTextField(); 
 	private int numOfAnswers = 4;
 	private JButton addQuestion = new JButton("Add Question");
+	private ButtonGroup options = new ButtonGroup();
+	private ArrayList<JRadioButton> correctAnswer = new ArrayList<>();
+	private int correctAnsIndex;
 	
 	
 	
@@ -41,10 +46,10 @@ public class CreateMCQPanel extends JPanel
 	/**
 	 * Add the contents to the panel.
 	 */
-	private void addToPanel(){
+	private void addToPanel() {
 		
 		add ( new JLabel("Create MCQ Question"), "wrap" );
-		add ( new JLabel("Question: "), "split 2");
+		add ( new JLabel("Question: ") );
 		
 		questionField.setPreferredSize( new Dimension(400,20));
 		add ( questionField, "wrap" );
@@ -67,9 +72,13 @@ public class CreateMCQPanel extends JPanel
 		for ( int i = 0; i < numOfAnswers; i++ )
 		{
 			answerFields.add( new JTextField() ); //Store the JTextField in the ArrayList.
-			add( answerFields.get(i), "wrap" ); //Add the JTextField to the panel.
+			add( answerFields.get(i), "split 2" ); //Add the JTextField to the panel.
 			answerFields.get(i).setPreferredSize( new Dimension(100, 10)); //Set the dimensions.
+			correctAnswer.add( new JRadioButton() );
+			options.add( correctAnswer.get(i) );
+			add ( correctAnswer.get(i), "wrap" );
 		}
+		
 		
 	}
 	
@@ -91,5 +100,32 @@ public class CreateMCQPanel extends JPanel
 	 */
 	public JButton getAddQuestionButton() {
 		return addQuestion;
+	}
+	
+	
+	
+	/**
+	 * Returns the correct answer which is assigned
+	 * depending on the radio button selected.
+	 * @return
+	 */
+	public String getCorrectAnswer() {
+
+		for (int i = 0; i < answerFields.size(); i++ ) {
+			if ( correctAnswer.get(i).isSelected()) 
+				return answerFields.get(i).toString();
+		}
+		return null;
+	}
+	
+	
+	
+	class addQuestion implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			
+			
+			
+		}
 	}
 }
