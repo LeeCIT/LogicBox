5f02ed4cdf7e4bf0c5316abcb3834f5f7292892f
Lee <lee.coakley@mycit.ie>
2014-03-16 19:39:19 +0000

    ToolManager: Progressing...

--- LogicBox/src/logicBox/gui/editor/tools/ToolManager.java
+++ LogicBox/src/logicBox/gui/editor/tools/ToolManager.java
@@ -18,14 +18,17 @@ import logicBox.gui.editor.EditorWorld;
  */
 public class ToolManager
 {
+	private ToolContextual  toolContextual;
 	private ToolDragger     toolDragger;
 	private ToolHighlighter toolHighlighter;
 	private ToolPlacer      toolPlacer;
 	private ToolSelector    toolSelector;
 	private ToolTraceDrawer toolTraceDrawer;
 	
-	private List<Tool>            tools;
 	private List<EditorComponent> selection;
+	
+	private List<Tool> tools;
+	private Tool       activeTool;
 	
 	
 	
@@ -33,38 +36,46 @@ public class ToolManager
 		selection = new ArrayList<>();
 		tools     = new ArrayList<>();
 		
-		toolDragger = add( new ToolDragger(panel, world, cam, this) );
+		setupTools ( panel, world, cam );
+		setActiveTool( toolContextual );
 	}
 	
 	
 	
-	public ToolDragger getDragger() {
-		return toolDragger;
+	public void setActiveTool( Tool tool ) {
+		for (Tool t: tools)
+			t.detach();
+		
+		activeTool = tool;
+		activeTool.attach();
 	}
 	
 	
 	
-	public ToolHighlighter getHighlighter() {
-		return toolHighlighter;
+	private void setupTools( EditorPanel panel, EditorWorld world, Camera cam ) {
+		toolContextual  = add( new ToolContextual (panel, world, cam, this) );
+		toolDragger     = add( new ToolDragger    (panel, world, cam, this) );
+		toolHighlighter = add( new ToolHighlighter(panel, world, cam, this) );
+		toolPlacer      = add( new ToolPlacer     (panel, world, cam, this) );
+		toolSelector    = add( new ToolSelector   (panel, world, cam, this) );
+		toolTraceDrawer = add( new ToolTraceDrawer(panel, world, cam, this) );
 	}
 	
 	
 	
-	public ToolPlacer getPlacer() {
-		return toolPlacer;
+	private <T extends Tool> T add( T tool ) {
+		tools.add( tool );
+		return tool;
 	}
 	
 	
 	
-	public ToolSelector getSelector() {
-		return toolSelector;
-	}
-	
-	
-	
-	public ToolTraceDrawer getTraceDrawer() {
-		return toolTraceDrawer;
-	}
+	public ToolContextual  getContextual()  { return toolContextual;  }
+	public ToolDragger     getDragger()     { return toolDragger;     }
+	public ToolHighlighter getHighlighter() { return toolHighlighter; }
+	public ToolPlacer      getPlacer()      { return toolPlacer;      }
+	public ToolSelector    getSelector()    { return toolSelector;    }
+	public ToolTraceDrawer getTraceDrawer() { return toolTraceDrawer; }
 	
 	
 	
@@ -74,14 +85,35 @@ public class ToolManager
 	
 	
 	
+	protected boolean hasSelection() {
+		return ! selection.isEmpty();
+	}
+	
+	
 	protected List<EditorComponent> getSelection() {
 		return selection;
 	}
-	
-	
-	
-	private <T extends Tool> T add( T tool ) {
-		tools.add( tool );
-		return tool;
-	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
