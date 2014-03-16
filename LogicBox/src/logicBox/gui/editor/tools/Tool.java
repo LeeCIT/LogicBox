5cdea2104c036e3c1033b0f91cf053c12001abaa
Lee <lee.coakley@mycit.ie>
2014-03-16 21:37:46 +0000

    Tool: Pulled up common derived methods

--- LogicBox/src/logicBox/gui/editor/tools/Tool.java
+++ LogicBox/src/logicBox/gui/editor/tools/Tool.java
@@ -3,9 +3,14 @@
 
 package logicBox.gui.editor.tools;
 
+import java.awt.event.MouseEvent;
+import javax.swing.SwingUtilities;
 import logicBox.gui.editor.Camera;
+import logicBox.gui.editor.EditorComponent;
 import logicBox.gui.editor.EditorPanel;
 import logicBox.gui.editor.EditorWorld;
+import logicBox.util.Geo;
+import logicBox.util.Vec2;
 
 
 
@@ -21,6 +26,7 @@ public abstract class Tool
 	protected Camera      cam;
 	
 	private boolean attached;
+	private double  dragThreshold = 4;
 	
 	
 	
@@ -50,4 +56,54 @@ public abstract class Tool
 	
 	
 	public abstract void detach();
+	
+	
+	
+	protected boolean isLeft( MouseEvent ev ) {
+		return SwingUtilities.isLeftMouseButton( ev );
+	}
+	
+	
+	
+	protected boolean isRight( MouseEvent ev ) {
+		return SwingUtilities.isRightMouseButton( ev );
+	}
+	
+	
+	
+	protected boolean isDragThresholdMet( Vec2 origin, Vec2 now ) {
+		return Geo.distance(origin,now) >= (dragThreshold / cam.getZoom());
+	}
+	
+	
+	
+	protected boolean isComponentAt( Vec2 pos ) {
+		return null != world.findTopmostAt( pos );
+	}
+	
+	
+	
+	protected EditorComponent getComponentAt( Vec2 pos ) {
+		return world.findTopmostAt( pos );
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
