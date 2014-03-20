04c093c3dbf790f41c07e48a7919c18d7ed5fbe5
ShaunCIT <shaun.odonovan@mycit.ie>
2014-03-20 10:57:39 -0700

    Class added

---
+++ LogicBox/src/HelpMenuPrototype/ComponentInfo.java
@@ -0,0 +1,18 @@
+package HelpMenuPrototype;
+
+
+
+public class ComponentInfo 
+{
+	
+	private String compName;
+	private String compDes;
+	
+	public ComponentInfo( String compName, String compDes ) {
+		this.compDes  = compDes;
+		this.compName = compName;
+	}
+	
+	
+	
+}
