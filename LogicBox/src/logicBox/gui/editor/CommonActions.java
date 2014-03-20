dac3a46b2e831614e880fab9dd1af905237e92ad
Lee <lee.coakley@mycit.ie>
2014-03-20 21:44:06 +0000

    CommonActions: Moved to editor package, added comments

--- LogicBox/src/logicBox/gui/editor/CommonActions.java
+++ LogicBox/src/logicBox/gui/editor/CommonActions.java
@@ -1,23 +1,27 @@
 
 
 
-package logicBox.gui;
+package logicBox.gui.editor;
 
 import java.io.File;
-import javax.swing.JFrame;
-
 import logicBox.fileManager.FileOpen;
+import logicBox.gui.GUI;
 import logicBox.gui.printing.EditorPrinter;
 
 
 
+/**
+ * Contains actions common to shortcuts, buttons and menu items.
+ * Things like opening files, printing and so on.
+ * @author John Murphy
+ */
 public abstract class CommonActions
 {
 	/**
 	 * When a file is opened this is called and the JFile chooser is brought up
 	 * @return	The file that was picked
 	 */
-	public static File openFile( JFrame frame ) {
+	public static File openFile( EditorFrame frame ) {
 		FileOpen file = new FileOpen( GUI.getMainFrame() );
 		return file.getPickedFile();
 	}
@@ -27,7 +31,7 @@ public abstract class CommonActions
 	/**
 	 * When the print function is called
 	 */
-	public static void print() {
+	public static void print( EditorFrame frame ) {
 		EditorPrinter print = new EditorPrinter();
 		print.setUpPrintJob( GUI.getMainFrame() );
 	}
