8862f8bdd74d094fa9092304a0a1ebcb46f92fae
Lee <lee.coakley@mycit.ie>
2014-03-20 23:45:16 +0000

    CommonActions: Now adds actions to AbstractButtons

--- LogicBox/src/logicBox/gui/editor/CommonActions.java
+++ LogicBox/src/logicBox/gui/editor/CommonActions.java
@@ -3,9 +3,11 @@
 
 package logicBox.gui.editor;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
+import javax.swing.AbstractButton;
 import logicBox.fileManager.FileOpen;
-import logicBox.gui.GUI;
 import logicBox.gui.printing.EditorPrinter;
 
 
@@ -14,16 +16,24 @@ import logicBox.gui.printing.EditorPrinter;
  * Contains actions common to shortcuts, buttons and menu items.
  * Things like opening files, printing and so on.
  * @author John Murphy
+ * @author Lee Coakley
  */
 public abstract class CommonActions
 {
 	/**
 	 * When a file is opened this is called and the JFile chooser is brought up
-	 * @return	The file that was picked
+	 * @return The file that was picked
 	 */
-	public static File openFile( EditorFrame frame ) {
-		FileOpen file = new FileOpen( GUI.getMainFrame() );
-		return file.getPickedFile();
+	public static void addOpenCircuitListener( AbstractButton abutt, final EditorFrame frame ) {
+		abutt.addActionListener( new ActionListener() {
+			public void actionPerformed( ActionEvent ev ) {
+				FileOpen fileOpen = new FileOpen( frame );
+				File     file     = fileOpen.getPickedFile();
+				
+				if (file != null)
+					frame.getEditorPanel().getWorld().loadCircuit( file );
+			}
+		});
 	}
 	
 	
@@ -31,8 +41,21 @@ public abstract class CommonActions
 	/**
 	 * When the print function is called
 	 */
-	public static void print( EditorFrame frame ) {
-		EditorPrinter print = new EditorPrinter();
-		print.setUpPrintJob( GUI.getMainFrame() );
+	public static void addPrintCircuitListener( AbstractButton abutt, final EditorFrame frame ) {
+		abutt.addActionListener( new ActionListener() {
+			public void actionPerformed( ActionEvent ev ) {
+				new EditorPrinter().setUpPrintJob( frame );;
+			}
+		});
+	}
+	
+	
+	
+	public static void addRecentreCameraListener( AbstractButton abutt, final EditorFrame frame ) {
+		abutt.addActionListener( new ActionListener() {
+			public void actionPerformed( ActionEvent ev ) {
+				frame.getEditorPanel().recentreCamera();
+			}
+		});
 	}
 }
