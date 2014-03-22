02e32c6f2dd711b2bc3f926d874ef0ac05b2a935
Lee <lee.coakley@mycit.ie>
2014-03-22 19:00:10 +0000

    ToolManager: Changed way tools are managed

--- LogicBox/src/logicBox/gui/editor/tools/ToolManager.java
+++ LogicBox/src/logicBox/gui/editor/tools/ToolManager.java
@@ -7,8 +7,13 @@ import java.util.ArrayList;
 import java.util.List;
 import logicBox.gui.editor.Camera;
 import logicBox.gui.editor.EditorComponent;
+import logicBox.gui.editor.EditorCreationCommand;
 import logicBox.gui.editor.EditorPanel;
 import logicBox.gui.editor.EditorWorld;
+import logicBox.gui.editor.GraphicComActive;
+import logicBox.sim.component.ComponentActive;
+import logicBox.util.CallbackParam;
+import logicBox.util.Vec2;
 
 
 
@@ -18,31 +23,70 @@ import logicBox.gui.editor.EditorWorld;
  */
 public class ToolManager
 {
+	private EditorPanel     panel;
 	private ToolContextual  toolContextual;
 	private ToolHighlighter toolHighlighter;
 	private ToolPlacer      toolPlacer;
 	private ToolTraceDrawer toolTraceDrawer;
 	
 	private List<Tool> tools;
-	private Tool       activeTool;
 	
 	
 	
-	public ToolManager( EditorPanel panel, EditorWorld world, Camera cam ) {
-		tools = new ArrayList<>();
+	public ToolManager( EditorPanel panel ) {
+		this.panel = panel;
+		this.tools = new ArrayList<>();
 		
-		setupTools ( panel, world, cam );
-		setActiveTool( toolContextual );
+		setupTools ( panel, panel.getWorld(), panel.getCamera() );
+		takeExclusiveControl( toolTraceDrawer );
 	}
 	
 	
 	
-	public void setActiveTool( Tool tool ) {
+	public ToolContextual  getContextual()  { return toolContextual;  }
+	public ToolHighlighter getHighlighter() { return toolHighlighter; }
+	public ToolPlacer      getPlacer()      { return toolPlacer;      }
+	public ToolTraceDrawer getTraceDrawer() { return toolTraceDrawer; }
+	
+	
+	
+	public void takeExclusiveControl( Tool tool ) {
+		detachAll();
+		tool.attach();
+	}
+	
+	
+	
+	public void takeCooperativeControl( Tool a, Tool b ) {
+		detachAll();
+		a.attach();
+		b.attach();
+	}
+	
+	
+	
+	public void releaseControl() {
+		takeCooperativeControl( toolContextual, toolHighlighter );
+	}
+	
+	
+	
+	public void initiateComponentCreation( final EditorCreationCommand ecc ) {
+		getPlacer().placementStart( ecc.getGraphicPreview(), new CallbackParam<Vec2>() {
+			public void execute( Vec2 pos ) {
+				ComponentActive  scom = ecc.getComponentPayload();
+				GraphicComActive gca  = scom.getGraphic();
+				EditorComponent  ecom = new EditorComponent( scom, gca, pos );
+				panel.getWorld().add( ecom );
+			}
+		});
+	}
+	
+	
+	
+	private void detachAll() {
 		for (Tool t: tools)
 			t.detach();
-		
-		activeTool = tool;
-		activeTool.attach();
 	}
 	
 	
@@ -60,13 +104,6 @@ public class ToolManager
 		tools.add( tool );
 		return tool;
 	}
-	
-	
-	
-	public ToolContextual  getContextual()  { return toolContextual;  }
-	public ToolHighlighter getHighlighter() { return toolHighlighter; }
-	public ToolPlacer      getPlacer()      { return toolPlacer;      }
-	public ToolTraceDrawer getTraceDrawer() { return toolTraceDrawer; }
 }
 
 
