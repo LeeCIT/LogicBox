c23f86e34f084c57bb3bf9ef26e708efe872e53e
Lee <lee.coakley@mycit.ie>
2014-03-24 18:29:57 +0000

    EditorToolBarButton: Now much smaller and draws border on hover

--- LogicBox/src/logicBox/gui/editor/toolbar/EditorToolBarButton.java
+++ LogicBox/src/logicBox/gui/editor/toolbar/EditorToolBarButton.java
@@ -3,19 +3,53 @@
 
 package logicBox.gui.editor.toolbar;
 
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import javax.swing.Icon;
 import javax.swing.JButton;
 
 
 
 /**
- * Makes buttons for the Editor toolbar
+ * Button for the Editor toolbar
  * @author John
  */
 public class EditorToolBarButton extends JButton
 {
 	public EditorToolBarButton( Icon icon, String tooltip ) {
-		super( icon );
+		super();
+		
+		if (icon != null)
+			setIcon( icon );
+		
 		setToolTipText( tooltip );
+		setMaximumSize( new Dimension(25,23) );
+		setFocusable( false );
+		
+		
+		addRolloverListener();
+	}
+	
+	
+	
+	private void addRolloverListener() {
+		addMouseListener( new MouseAdapter() {
+			public void mouseExited( MouseEvent e ) {
+				getModel().setRollover( false );
+			}
+			
+			public void mouseEntered( MouseEvent e ) {
+				getModel().setRollover( true );
+			}
+		});
+	}
+	
+	
+	
+	protected void paintBorder( Graphics g ) {
+		if (getModel().isRollover())
+			super.paintBorder( g );
 	}
 }
