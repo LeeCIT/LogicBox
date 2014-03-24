771136850361e4b8d201a8e3471d3ceca60477f7
Lee <lee.coakley@mycit.ie>
2014-03-24 21:39:59 +0000

    EditorPrinter: Refactored; now prints immediately

--- LogicBox/src/logicBox/gui/printing/EditorPrinter.java
+++ LogicBox/src/logicBox/gui/printing/EditorPrinter.java
@@ -1,49 +1,59 @@
+
+
+
 package logicBox.gui.printing;
 
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.print.*;
-import javax.swing.*;
+import logicBox.gui.editor.EditorFrame;
 
 
-public class EditorPrinter implements Printable{
+
+/**
+ * Creates a printing dialogue which prints the editor panel associated with the given frame.
+ * @author John Murphy
+ * @author Lee Coakley
+ */
+public class EditorPrinter implements Printable
+{
+	private EditorFrame	frame;
 	
-	JFrame frame;
-
-
-	public int print(Graphics graphics, PageFormat pageFormat, int pageIndex) throws PrinterException {
-		
-		// Important. If this is not here then it spools forever
-		if (pageIndex > 0) {
-            return NO_SUCH_PAGE;
-        }
+	
+	
+	public EditorPrinter( EditorFrame frame ) {
+		this.frame = frame;
+		createPrintJob();
+	}
+	
+	
+	
+	public int print( Graphics graphics, PageFormat pageFormat, int pageIndex ) throws PrinterException {
+		if (pageIndex > 0) // Important. If this is not here then it spools forever
+			return NO_SUCH_PAGE;
 		
 		Graphics2D g2d = (Graphics2D) graphics;
+		g2d.translate( pageFormat.getImageableX(), pageFormat.getImageableY() );
 		
-		g2d.translate(pageFormat.getImageableX(), pageFormat.getImageableY());
-		
-		// Print the frame
-		frame.print(graphics);
+		frame.getEditorPanel().print( graphics );
 		
 		return PAGE_EXISTS;
 	}
 	
 	
 	
-	
-	 public void setUpPrintJob(JFrame frame) {
-		 this.frame = frame;
-		 
-		 PrinterJob job = PrinterJob.getPrinterJob();
-         job.setPrintable(this);
-         boolean printOk = job.printDialog();
-         if (printOk) {
-             try {
-                  job.print();
-             } catch (PrinterException ex) {
-              ex.printStackTrace();
-             }
-         }
-	 }
-
+	private void createPrintJob() {
+		PrinterJob job = PrinterJob.getPrinterJob();
+		job.setPrintable( this );
+		boolean userSelectedOkay = job.printDialog();
+		
+		if (userSelectedOkay) {
+			try {
+				job.print();
+			}
+			catch (PrinterException ex) {
+				ex.printStackTrace();
+			}
+		}
+	}
 }
