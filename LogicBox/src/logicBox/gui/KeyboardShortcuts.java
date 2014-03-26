1ec33cc334ef4a0689e341995953335c0686815f
JohnCIT <john.murphy7@mycit.ie>
2014-03-26 19:22:43 +0000

    KeyboardShortcuts: Added print

--- LogicBox/src/logicBox/gui/KeyboardShortcuts.java
+++ LogicBox/src/logicBox/gui/KeyboardShortcuts.java
@@ -1,13 +1,21 @@
+
 package logicBox.gui;
 
 import java.awt.event.ActionEvent;
-
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
 import javax.swing.AbstractAction;
 import javax.swing.KeyStroke;
 import logicBox.gui.editor.EditorPanel;
+import logicBox.gui.edtior.printing.EditorPrinter;
 import logicBox.gui.help.HelpFrame;
 
 
+/**
+ * Stores the keyboard shortcuts
+ * @author John
+ *
+ */
 public class KeyboardShortcuts {
 	
 	private EditorPanel panel;
@@ -16,12 +24,29 @@ public class KeyboardShortcuts {
 		this.panel = editorPanel;
 		
 		setUpHelpFrameAction();
+		setUpPrinting();
 		
 		
 	}
 	
 	
 	
+	/**
+	 * Printing shortcut
+	 */
+	private void setUpPrinting() {
+		String print = "print";
+		
+		panel.getInputMap().put( KeyStroke.getKeyStroke( "control P" ), print );
+		panel.getActionMap().put(print, new AbstractAction() {
+			public void actionPerformed(ActionEvent e) {
+				new EditorPrinter( GUI.getMainFrame() );
+			}
+		});	
+	}
+
+
+
 	/**
 	 * Sets up the keyboard shortcut for help
 	 */
