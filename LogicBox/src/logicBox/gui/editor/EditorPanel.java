a0c3d8a0e0ee0f415c9c7c166dab83da647704cf
Lee <lee.coakley@mycit.ie>
2014-03-28 15:25:29 +0000

    EditorPanel: No longer contains main logic, no just displays stuff

--- LogicBox/src/logicBox/gui/editor/EditorPanel.java
+++ LogicBox/src/logicBox/gui/editor/EditorPanel.java
@@ -5,25 +5,12 @@ package logicBox.gui.editor;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseMotionAdapter;
-import java.awt.event.MouseMotionListener;
+import java.util.List;
 import java.util.Set;
 import javax.swing.JPanel;
 import logicBox.gui.Gfx;
-import logicBox.gui.editor.tools.ToolManager;
-import logicBox.sim.component.Demux;
-import logicBox.sim.component.GateAnd;
-import logicBox.sim.component.GateBuffer;
-import logicBox.sim.component.GateNand;
-import logicBox.sim.component.GateNor;
-import logicBox.sim.component.GateNot;
-import logicBox.sim.component.GateOr;
-import logicBox.sim.component.GateXnor;
-import logicBox.sim.component.GateXor;
-import logicBox.sim.component.Mux;
 import logicBox.util.Bbox2;
-import logicBox.util.Callback;
+import logicBox.util.Evaluator;
 import logicBox.util.Geo;
 import logicBox.util.Util;
 import logicBox.util.Vec2;
@@ -34,14 +21,12 @@ import logicBox.util.Vec2;
  * The main simulation editor interface.
  * @author Lee Coakley
  */
-public class EditorPanel extends JPanel implements HistoryListener<EditorWorld>
+public class EditorPanel extends JPanel
 {
-	private Camera                      cam;
-	private EditorWorld                 world;
-	private ToolManager                 toolManager;
-	private HistoryManager<EditorWorld> historyManager;
-	
 	private Set<RepaintListener> repaintListeners;
+	
+	private Camera cam;
+	private Evaluator<List<Graphic>> evalViewables;
 	
 	private boolean enableGrid;
 	private boolean enableAntialiasing;
@@ -52,108 +37,21 @@ public class EditorPanel extends JPanel implements HistoryListener<EditorWorld>
 	public EditorPanel() {
 		super( true );
 		
-		enableGrid         = true;
-		enableAntialiasing = true;
-		repaintListeners   = Util.createIdentityHashSet();
-		
-		world = new EditorWorld();
-		
-		cam = new Camera( this );
-		cam.addTransformCallback( createOnTransformCallback() );
-		
-		historyManager = new HistoryManager<>( this );
-		historyManager.markChange();
-		
-		toolManager = new ToolManager( this );
+		this.enableGrid         = true;
+		this.enableAntialiasing = true;
+		this.repaintListeners   = Util.createIdentityHashSet();
 	}
 	
 	
 	
-	public void addDebugAndDemoStuff() {
-		world.add( new EditorComponent( new GateBuffer(), GraphicGen.generateGateBuffer(), new Vec2(  0, -128) ) );
-		world.add( new EditorComponent( new GateNot(),    GraphicGen.generateGateNot(),    new Vec2(  0, -256) ) );
-		
-		historyManager.markChange();
-		
-		for (int i=2; i<=4; i++) {
-			double xo = 192 * (i-2);
-			
-			world.add( new EditorComponent( new GateAnd(i),  GraphicGen.generateGateAnd(i),  new Vec2(xo, 0)   ) );
-			world.add( new EditorComponent( new GateNand(i), GraphicGen.generateGateNand(i), new Vec2(xo, 128) ) );
-			world.add( new EditorComponent( new GateOr(i),   GraphicGen.generateGateOr(i),   new Vec2(xo, 256) ) );
-			world.add( new EditorComponent( new GateNor(i),  GraphicGen.generateGateNor(i),  new Vec2(xo, 384) ) );
-			world.add( new EditorComponent( new GateXor(i),  GraphicGen.generateGateXor(i),  new Vec2(xo, 512) ) );
-			world.add( new EditorComponent( new GateXnor(i), GraphicGen.generateGateXnor(i), new Vec2(xo, 640) ) );
-		}
-		
-		historyManager.markChange();
-		
-		for (int i=2; i<=8; i++) {
-			double xo = 192 * (i-2);
-			
-			world.add( new EditorComponent( new Mux(i),   new Mux(i)  .getGraphic(), new Vec2(xo,-512) ) );
-			world.add( new EditorComponent( new Demux(i), new Demux(i).getGraphic(), new Vec2(xo,-768) ) );
-		}
-		
-		historyManager.markChange();
-		
-		addMouseOverTest();
+	public void setCamera( Camera cam ) {
+		this.cam = cam;
 	}
 	
 	
 	
-	public EditorWorld getHistoryState() {
-		return world;
-	}
-	
-	
-	
-	public void setStateFromHistory( EditorWorld world ) {
-		this.world = world;
-		this.world.clearGraphicSelectionAndHighlightStates();
-		repaint();
-	}
-	
-	
-	
-	public void recentreCamera() {
-		cam.interpolateToBbox( world.getWorldExtent(), 64, 3 );
-	}
-	
-	
-	
-	public Camera getCamera() {
-		return cam;
-	}
-	
-	
-	
-	public EditorWorld getWorld() {
-		return world;
-	}
-	
-	
-	
-	public ToolManager getToolManager() {
-		return toolManager;
-	}
-	
-	
-	
-	public HistoryManager<EditorWorld> getHistoryManager() {
-		return historyManager;
-	}
-	
-	
-	
-	public Bbox2 getWorldViewArea() {
-		return cam.getWorldViewArea();
-	}
-	
-	
-	
-	public Bbox2 getWorldExtent() {
-		return world.getWorldExtent();
+	public void setViewablesEvaluator( Evaluator<List<Graphic>> eval ) {
+		this.evalViewables = eval;
 	}
 	
 	
@@ -196,23 +94,8 @@ public class EditorPanel extends JPanel implements HistoryListener<EditorWorld>
 	
 	
 	
-	private void addMouseOverTest() {
-		addMouseMotionListener( new MouseMotionAdapter() {
-			public void mouseMoved( MouseEvent ev ) {
-				for (EditorComponent ecom: world.find( cam.getMousePosWorld() )) {
-					GraphicPinMapping gpm = ecom.graphic.findClosestPin( cam.getMousePosWorld(), 5 );
-					System.out.println( "Ed: " + ecom.com.getName() );
-					System.out.println( "Ed: " + gpm );					
-				}
-			}
-		});
-	}
-	
-	
-	
 	public void print( Graphics g ) {
-		// TODO change background colour, zoom out etc
-		System.out.println( "Printing " + this );
+		// TODO zoom out etc
 		isPrinting = true;
 		super.print( g );
 		isPrinting = false;
@@ -230,9 +113,9 @@ public class EditorPanel extends JPanel implements HistoryListener<EditorWorld>
 		Gfx.pushMatrix( g );
 			Gfx.pushAntialiasingStateAndSet( g, enableAntialiasing );
 				fillBackground( g );
-				cam.applyTransform( g );
+				applyCameraTransform( g );
 				drawGrid( g );
-				drawEditorComponents( g );
+				drawComponentGraphics( g );
 				drawRepaintListeners( g );
 			Gfx.popAntialiasingState( g );
 		Gfx.popMatrix( g );
@@ -240,9 +123,18 @@ public class EditorPanel extends JPanel implements HistoryListener<EditorWorld>
 	
 	
 	
-	private void drawEditorComponents( Graphics2D g ) {
-		for (EditorComponent ecom: world.getViewableComponents( cam, 64 )) {
-			Graphic graphic     = ecom.getGraphic();
+	private void applyCameraTransform( Graphics2D g ) {
+		if (cam != null)
+			cam.applyTransform( g );
+	}
+	
+	
+	
+	private void drawComponentGraphics( Graphics2D g ) {
+		if (evalViewables == null)
+			return;
+		
+		for (Graphic graphic: evalViewables.evaluate()) {
 			boolean wasInverted = graphic.isInverted();
 			
 			graphic.setInverted( isPrinting );
@@ -302,48 +194,6 @@ public class EditorPanel extends JPanel implements HistoryListener<EditorWorld>
 		
 		if (disableAA)
 			Gfx.popAntialiasingState( g );
-	}
-	
-	
-	
-	private Callback createOnTransformCallback() {
-		return new Callback() {
-			public void execute() {
-				onTransform();
-			}
-		};
-	}
-	
-	
-	
-	/**
-	 * When the transform is changed by the camera, repaint and generate mouse events.
-	 * The events ensure that tools update their positions when zooming/panning while dragging/placing/etc.
-	 */
-	private void onTransform() {
-		generateMouseEvent();
-		repaint();
-	}
-	
-	
-	
-	private void generateMouseEvent() {
-		MouseEvent me = new MouseEvent(
-			this,
-			MouseEvent.MOUSE_MOVED,
-			System.nanoTime(),
-			0,
-			(int) cam.getMousePosScreen().x,
-			(int) cam.getMousePosScreen().y,
-			0,
-			false,
-			MouseEvent.NOBUTTON
-		);
-		
-		for (MouseMotionListener ml: getMouseMotionListeners()) {
-			ml.mouseMoved  ( me );
-			ml.mouseDragged( me );
-		}
 	}
 }
 
