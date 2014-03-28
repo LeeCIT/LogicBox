5d53f2149825e182981fcd7b0eecf2c7c711f1db
Lee <lee.coakley@mycit.ie>
2014-03-28 19:37:41 +0000

    ToolboxButton: Added constructors; no longer abstract

--- LogicBox/src/logicBox/gui/editor/toolbox/ToolboxButton.java
+++ LogicBox/src/logicBox/gui/editor/toolbox/ToolboxButton.java
@@ -3,6 +3,8 @@
 
 package logicBox.gui.editor.toolbox;
 
+import javax.swing.Action;
+import javax.swing.Icon;
 import javax.swing.JButton;
 import logicBox.gui.editor.tools.ToolManager;
 import logicBox.util.Evaluator;
@@ -13,9 +15,39 @@ import logicBox.util.Evaluator;
  * A button specialised for use in the Toolbox class.
  * @author Lee Coakley
  */
-public abstract class ToolboxButton extends JButton
+public class ToolboxButton extends JButton
 {
 	private Evaluator<ToolManager> targetEvaluator;
+	
+	
+	
+	public ToolboxButton() {
+		super();
+	}
+	
+	
+	
+	public ToolboxButton( Action a ) {
+		super( a );
+	}
+	
+	
+	
+	public ToolboxButton( Icon icon ) {
+		super( icon );
+	}
+	
+	
+	
+	public ToolboxButton( String text, Icon icon ) {
+		super( text, icon );
+	}
+	
+	
+	
+	public ToolboxButton( String text ) {
+		super( text );
+	}
 	
 	
 	
