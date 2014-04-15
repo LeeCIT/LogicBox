a8994e6b0f3ebc37e72bf810a8eb7dca04c611a5
Lee <lee.coakley@mycit.ie>
2014-04-15 15:20:48 +0100

    EditorComponentTrace: Added getComponent()

--- LogicBox/src/logicBox/gui/editor/EditorComponentTrace.java
+++ LogicBox/src/logicBox/gui/editor/EditorComponentTrace.java
@@ -5,8 +5,7 @@ package logicBox.gui.editor;
 
 import java.awt.Graphics2D;
 import java.util.List;
-import logicBox.sim.component.Component;
-import logicBox.util.Line2;
+import logicBox.sim.component.Trace;
 import logicBox.util.Vec2;
 
 
@@ -19,13 +18,20 @@ public class EditorComponentTrace extends EditorComponent
 {
 	private static final long serialVersionUID = 1L;
 	
-	private GraphicTrace graphic;
+	protected Trace        com;
+	private   GraphicTrace graphic;
 	
 	
 	
-	public EditorComponentTrace( Component com, List<Vec2> points ) {
+	public EditorComponentTrace( Trace com, GraphicPinMapping gpmA, GraphicPinMapping gpmB, List<Vec2> points ) {
 		super( com );
-		graphic = new GraphicTrace( points, null, null );
+		graphic = new GraphicTrace( points, gpmA, gpmB );
+	}
+	
+	
+	
+	public Trace getComponent() {
+		return com;
 	}
 	
 	
@@ -37,35 +43,36 @@ public class EditorComponentTrace extends EditorComponent
 	
 	
 	public void draw( Graphics2D g ) {
+		graphic.setPowered( com.getState() );
 		graphic.draw( g );
 	}
-
-
-
+	
+	
+	
 	public void setPos( Vec2 pos ) {
 		// Do nothing
 	}
-
-
-
+	
+	
+	
 	public Vec2 getPos() {
 		return new Vec2();
 	}
-
-
-
+	
+	
+	
 	public void setAngle( double angle ) {
 		// Do nothing
 	}
-
-
-
+	
+	
+	
 	public double getAngle() {
 		return 0;
 	}
-
-
-
+	
+	
+	
 	public GraphicPinMapping findPinNear( Vec2 pos, double radius ) {
 		return null;
 	}
