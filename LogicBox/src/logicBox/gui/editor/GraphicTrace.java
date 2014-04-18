d8fa03a6ab68c659bfe26945371f29488af37e16
Lee <lee.coakley@mycit.ie>
2014-04-18 19:11:46 +0100

    GraphicTrace: Can now be rebuilt from points

--- LogicBox/src/logicBox/gui/editor/GraphicTrace.java
+++ LogicBox/src/logicBox/gui/editor/GraphicTrace.java
@@ -27,28 +27,34 @@ public class GraphicTrace extends Graphic implements GraphicIntersector
 {
 	private static final long serialVersionUID = 1L;
 	
-	private List<Vec2>        points;
-	private List<Line2>       lines;
-	private VecPath           polyLine;
-	private boolean           isPowered;
-	private boolean 		  connectedSource;
-	private boolean 		  connectedDest;
+	private List<Vec2>  points;
+	private List<Line2> lines;
+	private VecPath     polyLine;
+	private boolean     isPowered;
+	private boolean 	connectedSource;
+	private boolean 	connectedDest;
 	
 	
 	
 	public GraphicTrace( List<Vec2> points, GraphicPinMapping gpmSrc, GraphicPinMapping gpmDest ) {
 		super();
+		setFromPoints( points );
+		setColors( false );
+	}
+	
+	
+	
+	public void setFromPoints( List<Vec2> points ) {
 		this.points   = new ArrayList<>( points );
 		this.lines    = Line2.toLines( points );
 		this.polyLine = new VecPath( points, false );
-		setColors( false );
 	}
 	
 	
 	
 	public void setPowered( boolean powered ) {
 		isPowered = powered;
-		setColors( powered );
+		setColors( isPowered );
 	}
 	
 	
@@ -70,6 +76,12 @@ public class GraphicTrace extends Graphic implements GraphicIntersector
 		
 		if (connectedSource) drawConnection( g, points.get(0)               );
 		if (connectedDest)   drawConnection( g, points.get(points.size()-1) );
+	}
+	
+	
+	
+	public List<Vec2> getPoints() {
+		return points;
 	}
 	
 	
