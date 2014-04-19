6c6e30bee22cc9b87d72f55d7e70126c213feac3
Lee <lee.coakley@mycit.ie>
2014-04-19 02:02:41 +0100

    EditorComponentLed: Implemented

--- LogicBox/src/logicBox/gui/editor/EditorComponentLed.java
+++ LogicBox/src/logicBox/gui/editor/EditorComponentLed.java
@@ -3,13 +3,41 @@
 
 package logicBox.gui.editor;
 
+import java.awt.Color;
+import logicBox.sim.component.DisplayLed;
+import logicBox.util.Vec2;
+
 
 
 /**
  * 
  * @author Lee Coakley
  */
-public class EditorComponentLED
+public class EditorComponentLed extends EditorComponentActive
 {
+	private static final long serialVersionUID = 1L;
 	
+	private DisplayLed com;
+	
+	
+	public EditorComponentLed( DisplayLed com, GraphicComActive gca, Vec2 pos, double angle ) {
+		super( com, gca, pos, angle );
+		this.com = com;
+		onWorldChange();
+	}
+	
+	
+	
+	public DisplayLed getComponent() {
+		return com;
+	}
+	
+	
+	
+	public void onWorldChange() {
+		boolean lit = com.isLit();
+		Color   col = (lit) ? EditorStyle.colLedOn : EditorStyle.colLedOff;
+
+		getGraphic().setFillOverride( true, col );
+	}
 }
