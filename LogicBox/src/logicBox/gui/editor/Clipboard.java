c03c91d7d13c928a398bb7a3a163a551f1cc9104
Lee <lee.coakley@mycit.ie>
2014-04-20 00:04:17 +0100

    Clipboard: Added comments and clear()

--- LogicBox/src/logicBox/gui/editor/Clipboard.java
+++ LogicBox/src/logicBox/gui/editor/Clipboard.java
@@ -21,7 +21,16 @@ public abstract class Clipboard
 	
 	
 	
+	/**
+	 * Set the current clipboard selection.
+	 * There is no need to modify it in any way, this is all done internally.
+	 */
 	public static void set( Selection sel ) {
+		if (selection.isEmpty()) {
+			clear();
+			return;
+		}
+		
 		Selection      copy = Util.deepCopy( sel );
 		Set<Component> coms = Util.createIdentityHashSet();
 		Simulation     tsim = new Simulation();
@@ -39,11 +48,20 @@ public abstract class Clipboard
 	
 	
 	
+	/**
+	 * Get a copy of the clipboard selection.
+	 */
 	public static Selection get() {
 		if (isEmpty())
 			throw new RuntimeException( "Clipboard is empty!" );
 		
 		return Util.deepCopy( selection );
+	}
+	
+	
+	
+	public static void clear() {
+		selection = null;
 	}
 	
 	
