ef0adfce38a9ab74172dc8f0b151b0be0976d88b
Lee <lee.coakley@mycit.ie>
2014-04-19 01:18:11 +0100

    Added EditorComponentJunction class

---
+++ LogicBox/src/logicBox/gui/editor/EditorComponentJunction.java
@@ -0,0 +1,72 @@
+
+
+
+package logicBox.gui.editor;
+
+import logicBox.sim.component.Junction;
+import logicBox.util.Vec2;
+
+
+
+/**
+ * A representation of a junction in the editor GUI.
+ * @author Lee Coakley
+ */
+public class EditorComponentJunction extends EditorComponent
+{
+	private static final long serialVersionUID = 1L;
+	
+	private Junction        com;
+	private GraphicJunction graphic;
+	
+	
+	
+	public EditorComponentJunction( Junction com, Vec2 pos ) {
+		super( com );
+		this.com     = com;
+		this.graphic = new GraphicJunction( pos );
+	}
+	
+	
+	
+	public Junction getComponent() {
+		return com;
+	}
+	
+	
+	
+	public GraphicJunction getGraphic() {
+		return graphic;
+	}
+	
+	
+	
+	public void setPos( Vec2 pos ) {
+		graphic.setPos( pos );
+		signalTransformChange();
+	}
+	
+	
+	
+	public Vec2 getPos() {
+		return graphic.getPos();
+	}
+	
+	
+	
+	public void setAngle( double angle ) {
+		// Do nothing
+	}
+	
+	
+	
+	public double getAngle() {
+		return 0;
+	}
+	
+	
+	
+	public GraphicPinMapping findPinNear( Vec2 pos, double radius ) {
+		return null;
+	}
+}
