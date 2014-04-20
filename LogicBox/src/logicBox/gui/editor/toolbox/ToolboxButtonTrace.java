b5b7ea1e2c38ad4c3144853063102b76b2117302
Lee <lee.coakley@mycit.ie>
2014-04-20 02:51:42 +0100

    ToolboxButtonTrace: Moved graphics generating code into GraphicGen

--- LogicBox/src/logicBox/gui/editor/toolbox/ToolboxButtonTrace.java
+++ LogicBox/src/logicBox/gui/editor/toolbox/ToolboxButtonTrace.java
@@ -3,11 +3,7 @@
 
 package logicBox.gui.editor.toolbox;
 
-import java.util.ArrayList;
-import java.util.List;
-import logicBox.gui.editor.Graphic;
-import logicBox.gui.editor.GraphicTrace;
-import logicBox.util.Vec2;
+import logicBox.gui.editor.GraphicGen;
 
 
 
@@ -18,23 +14,8 @@ import logicBox.util.Vec2;
 public class ToolboxButtonTrace extends ToolboxButtonGraphic
 {
 	public ToolboxButtonTrace() {
-		super( makeGraphic(), "Trace" );
+		super( GraphicGen.generateTrace(), "Trace" );
 		setScaleAux( 0.75 );
-	}
-	
-	
-	
-	private static Graphic makeGraphic() {
-		List<Vec2> points = new ArrayList<>();
-		points.add( new Vec2( -24,   0) );
-		points.add( new Vec2(   0,  18) );
-		points.add( new Vec2(  14, -18) );
-		points.add( new Vec2(  28,   0) );
-		
-		GraphicTrace graphic = new GraphicTrace( points );
-		graphic.setConnectedSource( true );
-		graphic.setConnectedDest  ( true );
-		return graphic;
 	}
 }
 
