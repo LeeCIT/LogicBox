62ed4e12bc3d76f73bfdc12b151abaed9b10ef14
Lee <lee.coakley@mycit.ie>
2014-04-20 12:53:55 +0100

    EditorStyle: Changed LED-off colour to not look like vomit

--- LogicBox/src/logicBox/gui/editor/EditorStyle.java
+++ LogicBox/src/logicBox/gui/editor/EditorStyle.java
@@ -17,7 +17,7 @@ public abstract class EditorStyle
 {
 	public static Color colBackground            = new Color(  55,  37,  37 );
 	public static Color colGrid                  = new Color(  89,  51,  72 );
-	public static Color colLedOff                = new Color(  81,  70,  35 );
+	public static Color colLedOff                = new Color(  62,  31,   0 );
 	public static Color colLedOn                 = new Color( 248, 229, 104 );
 	public static Color colTraceOff              = new Color(  58, 122,  54 );
 	public static Color colTraceOn               = new Color( 146, 248, 122 );
