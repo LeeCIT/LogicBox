4f9830d3889d2cb6380e730e52e4632505c05fc9
Lee <lee.coakley@mycit.ie>
2014-04-20 03:13:17 +0100

    SourceOscillator: Added sync mechanism

--- LogicBox/src/logicBox/sim/component/SourceOscillator.java
+++ LogicBox/src/logicBox/sim/component/SourceOscillator.java
@@ -11,6 +11,8 @@ import logicBox.gui.editor.GraphicGen;
 /**
  * A clock generator.
  * To make the sim easier to program, its frequency is always a multiple of a common base frequency.
+ * All that needs to happen for everything to remain synced properly is for the sim to call
+ * sendClockSignal on all oscillators at the given base frequency.
  * @author Lee Coakley
  */
 public class SourceOscillator extends Source
@@ -18,25 +20,45 @@ public class SourceOscillator extends Source
 	private static final long serialVersionUID = 1L;
 	public  static final int  baseFrequencyHz  = 200;
 	
+	private int cycleCounter;
 	private int frequencyDivisor;
 	
 	
 	
 	public SourceOscillator( int frequencyDivisor ) {
 		super( false );
-		this.frequencyDivisor = frequencyDivisor;
+		setFrequencyDivisor( frequencyDivisor );
 	}
 	
 	
 	
-	public void toggleState() {
+	public synchronized void sendClockSignal() {
+		if (cycleCounter++ % frequencyDivisor == 0)
+			toggleState();
+	}
+	
+	
+	private void toggleState() {
 		setState( ! getState() );
+	}
+	
+	
+	
+	public void setFrequencyDivisor( int frequencyDivisor ) {
+		this.frequencyDivisor = frequencyDivisor;
 	}
 	
 	
 	
 	public int getFrequencyDivisor() {
 		return frequencyDivisor;
+	}
+	
+	
+	
+	public void reset() {
+		super.reset();
+		cycleCounter = 0;
 	}
 	
 	
