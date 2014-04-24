403e586eceaba74dffab522a15735fff71ca8ad4
Lee <lee.coakley@mycit.ie>
2014-04-24 15:09:47 +0100

    Added EditorComponentSevenSeg class

---
+++ LogicBox/src/logicBox/gui/editor/EditorComponentSevenSeg.java
@@ -0,0 +1,42 @@
+
+
+
+package logicBox.gui.editor;
+
+import logicBox.sim.component.DisplaySevenSeg;
+import logicBox.util.Vec2;
+
+
+
+/**
+ * Specialised for the LED.
+ * @author Lee Coakley
+ */
+public class EditorComponentSevenSeg extends EditorComponentActive
+{
+	private static final long serialVersionUID = 1L;
+	
+	
+	
+	public EditorComponentSevenSeg( DisplaySevenSeg com, Vec2 pos, double angle ) {
+		super( com, GraphicGen.generateDisplaySevenSeg(), pos, angle );
+	}
+	
+	
+	
+	public DisplaySevenSeg getComponent() {
+		return (DisplaySevenSeg) super.getComponent();
+	}
+	
+	
+	
+	public GraphicSevenSeg getGraphic() {
+		return (GraphicSevenSeg) super.getGraphic();
+	}
+	
+	
+	
+	public void onWorldChange() {
+		getGraphic().setSegmentStates( getComponent().getSegmentStates() );
+	}
+}
