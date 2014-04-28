a3c10ec0a8e22346d78f2372b14f2a0d1d19bacb
Lee <lee.coakley@mycit.ie>
2014-04-28 16:50:09 +0100

    Added AboutDialogue class placeholder

---
+++ LogicBox/src/logicBox/gui/help/AboutDialogue.java
@@ -0,0 +1,18 @@
+
+
+
+package logicBox.gui.help;
+
+import javax.swing.JDialog;
+
+
+
+/**
+ * Shows information about the program.
+ * @author Lee Coakley
+ * TODO implement, put some fancy animated graphic in, etc
+ */
+public class AboutDialogue extends JDialog
+{
+	
+}
