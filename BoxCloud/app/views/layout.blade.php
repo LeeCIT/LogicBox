14b33745f1fca1d423c6b8583b6dffa4c28be0ed
Robert O' Leary <jatochnietdans@gmail.com>
2014-05-10 13:56:46 +0100

    Added Google Analytics tracking code

--- BoxCloud/app/views/layout.blade.php
+++ BoxCloud/app/views/layout.blade.php
@@ -16,6 +16,16 @@
 
     <!-- Custom styles for this template -->
     <link href="/assets/css/main.css" rel="stylesheet">
+	
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+	
+	  ga('create', 'UA-50848050-1', 'logicbox.info');
+	  ga('send', 'pageview');
+	</script>
   </head>
 
   <body>
